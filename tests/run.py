3e3eb0deea411327dcd6369ac48955556f7b9a4a
Ville Suoranta <vsuorant@nrao.edu>
2018-11-16 14:06:25 -0500

    Add missing "greater than" to run.py

--- tests/run.py
+++ tests/run.py
@@ -39,7 +39,7 @@ def test_result_to_xml (result):
     testxml = '<testcase classname="' + testname + '"' \
           + ' name="Run regression" time="' + str(round(run_time)) + '">'
     if ( returncode != 0) :
-       testxml = testxml + '<failure message="' + xml_escape(str(b''.join(tail(testerr,"10")))) + '"</failure>'
+       testxml = testxml + '<failure message="' + xml_escape(str(b''.join(tail(testerr,"10")))) + '"></failure>'
     testxml = testxml + '</testcase>\n'
     return testxml
 
