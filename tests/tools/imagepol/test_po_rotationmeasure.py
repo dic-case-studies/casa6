11cfbd7af875388d8172445609f786884af99c3d
David Mehringer <dmehring@nrao.edu>
2018-12-04 12:18:12 -0500

    CAS-12059 split tool tests out

--- tests/tools/imagepol/test_po_rotationmeasure.py
+++ tests/tools/imagepol/test_po_rotationmeasure.py
@@ -1,77 +1,11 @@
-##########################################################################
-# imfit_test.py
-#
-# Copyright (C) 2008, 2009
-# Associated Universities, Inc. Washington DC, USA.
-#
-# This script is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Library General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at your
-# option) any later version.
-#
-# This library is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
-# License for more details.
-#
-# You should have received a copy of the GNU Library General Public License
-# along with this library; if not, write to the Free Software Foundation,
-# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
-#
-# Correspondence concerning AIPS++ should be adressed as follows:
-#        Internet email: aips2-request@nrao.edu.
-#        Postal address: AIPS++ Project Office
-#                        National Radio Astronomy Observatory
-#                        520 Edgemont Road
-#                        Charlottesville, VA 22903-2475 USA
-#
-# <author>
-# Dave Mehringer
-# </author>
-#
-# <summary>
-# Test suite for the CASA tool method po.rotationmeasure
-# </summary>
-#
-# <reviewed reviwer="" date="" tests="" demos="">
-# </reviewed
-#
-# <prerequisite>
-# <ul>
-# </ul>
-# </prerequisite>
-#
-# <etymology>
-# Test for the po.rotationmeasure() tool method
-# </etymology>
-#
-# <synopsis>
-# Test the po.rotationmeasure() tool method
-# </synopsis> 
-#
-# <example>
-#
-# This test runs as part of the CASA python unit test suite and can be run from
-# the command line via eg
-# 
-# `echo $CASAPATH/bin/casa | sed -e 's$ $/$'` --nologger --log2term -c `echo $CASAPATH | awk '{print $1}'`/gcwrap/python/scripts/regressions/admin/runUnitTest.py test_po_rotationmeasure[test1,test2,...]
-#
-# </example>
-#
-# <motivation>
-# To provide a test standard for the ia.tofits() tool method to ensure
-# coding changes do not break the associated bits 
-# </motivation>
-#
-
-###########################################################################
 import shutil
 import unittest
+import math
 
+from casatools import constants
 from casatools import imagepol as potool
 from casatools import image as iatool
 from casatools import table
-from casatools import constants
 
 datapath='regression/unittest/po_tool/'
 eq_beams = datapath + "pol_eq_beams.fits"
@@ -91,7 +25,6 @@ class po_rotationmeasure_test(unittest.TestCase):
     def test_multibeam(self):
         """Test multibeam images for correct behavior"""
         mypo = self.mypo
-        print(eq_beams)
         mypo.open(eq_beams)
         self.assertTrue(mypo.rotationmeasure("g"))
         mypo.done()
@@ -99,41 +32,6 @@ class po_rotationmeasure_test(unittest.TestCase):
         self.assertRaises(Exception, mypo.rotationmeasure, "hh")
         mypo.done()
         
-    @unittest.skip("uses a task")
-    def test_rmfit_basics(self):
-        """Sanity tests for task rmfit"""
-        myia = iatool()
-        outfile = "xx.im"
-        myia.fromshape(outfile, [20, 20, 4, 20])
-        myia.addnoise()
-        myia.done()
-        myrm = "rm1.im"
-        self.assertTrue(rmfit(imagename=outfile, rm=myrm))
-        myia.open(myrm)
-        self.assertTrue((myia.shape() == [20, 20]).all())
-        got1 = myia.statistics(list=True, verbose=True)['sumsq']
-        myia.done()
-        
-        # test concatenation of images
-        outfile = "yy.im"
-        myia.fromshape(outfile, [20, 20, 4, 20])
-        myia.addnoise()
-        csys = myia.coordsys()
-        refval = csys.referencevalue()['numeric']
-        refval[3] = 1.5e9
-        csys.setreferencevalue(refval)
-        myia.setcoordsys(csys.torecord())
-        myia.done()
-        images = ["xx.im", "yy.im"]
-        myrm = "rm2.im"
-        self.assertTrue(rmfit(imagename=images, rm=myrm))
-        myia.open(myrm)
-        self.assertTrue((myia.shape() == [20, 20]).all())
-        got2 = myia.statistics(list=True, verbose=True)['sumsq']
-        myia.done()
-        self.assertTrue(abs(got1 - got2) > 0.1)
-
-    @unittest.skip("uses a task")
     def test_algorithm(self):
         """Test rotation measure computation algorithm"""
         myia = iatool()
@@ -148,14 +46,14 @@ class po_rotationmeasure_test(unittest.TestCase):
         # U values all 1
         U = 1
         pixvals[:,:,2,:] = U
-        c = constants.c
+        c = constants.c/100
         RM = 9.6
         pa0deg = 22.5
-        pa0 = pa0deg/180*pi
+        pa0 = pa0deg/180*math.pi
         for chan in range(myia.shape()[3]):
             freq = myia.toworld([0,0,0,chan])['numeric'][3]
             lam = c/freq
-            Q = U/tan(2*(pa0 + RM*lam*lam))
+            Q = U/math.tan(2*(pa0 + RM*lam*lam))
             pixvals[:,:,1,chan] = Q
         myia.putchunk(pixvals)
         myia.done()
@@ -163,23 +61,19 @@ class po_rotationmeasure_test(unittest.TestCase):
         rmim = "rm.im"
         pa0im = "pa0.im"
         sigma = 10e-8
-        for i in [0, 1]:
-            if i == 0:
-                mypo.open(imagename)
-                mypo.rotationmeasure(rm=rmim, pa0=pa0im, sigma=sigma)
-                mypo.done()
-            else:
-                rmfit(imagename=imagename, rm=rmim, pa0=pa0im, sigma=sigma)
-            myia.open(rmim)
-            stats = myia.statistics(list=True, verbose=True)
-            self.assertTrue((abs(stats['min'][0] - RM)) < 1e-4)
-            self.assertTrue((abs(stats['max'][0] - RM)) < 1e-4)
-            myia.done(remove=True)
-            myia.open(pa0im)
-            stats = myia.statistics(list=True, verbose=True)
-            self.assertTrue((abs(stats['min'][0] - pa0deg)) < 1e-4)
-            self.assertTrue((abs(stats['max'][0] - pa0deg)) < 1e-4)
-            myia.done(remove=True)
+        mypo.open(imagename)
+        mypo.rotationmeasure(rm=rmim, pa0=pa0im, sigma=sigma)
+        mypo.done()
+        myia.open(rmim)
+        stats = myia.statistics(list=True, verbose=True)
+        self.assertTrue((abs(stats['min'][0] - RM)) < 1e-4)
+        self.assertTrue((abs(stats['max'][0] - RM)) < 1e-4)
+        myia.done(remove=True)
+        myia.open(pa0im)
+        stats = myia.statistics(list=True, verbose=True)
+        self.assertTrue((abs(stats['min'][0] - pa0deg)) < 1e-4)
+        self.assertTrue((abs(stats['max'][0] - pa0deg)) < 1e-4)
+        myia.done(remove=True)
 
 def suite():
     return [po_rotationmeasure_test]
