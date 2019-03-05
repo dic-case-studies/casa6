a2069e6af728bc80ddc848c50c24861b4f33c4b7
Darrell Schiebel <darrell@schiebel.us>
2019-03-05 15:01:30 -0500

    unquote grpc flags for qmake

--- src/python/__main__.py
+++ src/python/__main__.py
@@ -3,9 +3,12 @@ import sys
 from .config import build
 
 for flag in sys.argv:
+    ## qmake has difficuties with quoted paths, e.g. '-I/some/path/for/grpc/include/files'
+    ## if such quoting is required (e.g. for paths with spaces in them), then probably
+    ## another -quoted flag should be introduced to change the default behavior
     if flag == '--grpc-link':
-        print(' '.join(map(lambda x: repr(x),build['build.flags.link.grpc'])))
+        print(' '.join(build['build.flags.link.grpc']))
     if flag == '--grpc-compile':
-        print(' '.join(map(lambda x: repr(x),build['build.flags.compile.grpc'])))
+        print(' '.join(build['build.flags.compile.grpc']))
     if flag == '--grpc-protoc':
         print(build['build.compiler.protoc'])
