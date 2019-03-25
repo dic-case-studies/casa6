1e8c28152e4c8f3ff63b8d83c8ce1a95fd869dfb
Des Small <small@jive.eu>
2019-03-25 10:15:06 +0100

    Fixed the FringeJones test for dispersive fringe fitting

--- code/synthesis/MeasurementComponents/test/tFringeJones_GT.cc
+++ code/synthesis/MeasurementComponents/test/tFringeJones_GT.cc
@@ -184,14 +184,14 @@ public:
 		   16, // ntime per scan
 		   false),  // unpolarized
     // nPar, 1, {1 | nAntennas}
-    fpar(6, 1, VisCalTestBase::nAnt, 0.0f)  // 6 pars per antenna
+    fpar(8, 1, VisCalTestBase::nAnt, 0.0f)  // Now 8 pars per antenna
   {
       // Add FringeJonesTest specific init
       //  e.g., fill fpar with interesting values
     for (Int i=1; i!=VisCalTestBase::nAnt; i++) {
       // 1, 4 are delay.
       fpar(1, 0, i) = 2.3;
-      fpar(4, 0, i) = -1.7;
+      fpar(1+4, 0, i) = -1.7;
       // Parameters 2 and 5 are rate.  But VisCal::setMeta currently
       // only supports a single double for time, and this meta data is
       // used to generate all the Jones matrices for the FringeJones
@@ -200,7 +200,7 @@ public:
       // intervene on this, so for now we can only test zero rates for
       // the FringeJones class proper.
       fpar(2, 0, i) = 0.0;
-      fpar(5, 0, i) = 0.0;
+      fpar(2+4, 0, i) = 0.0;
     }
     // uncomment to see data shape summary from
     //VisCalTestBase::summary("FringeJonesTest");  
@@ -323,6 +323,7 @@ TEST_F(FringeJonesTest, FringeJones_selfSolveOneTest) {
 
   // Add comparison tests here
   Matrix<Float> p = FJsol.solveRPar().nonDegenerate();
+  cerr << p.shape() << endl;
   Float delay1 = 2.3;
   Float delay2 = -1.7;
   Float rate1 = 0.0;
@@ -330,16 +331,16 @@ TEST_F(FringeJonesTest, FringeJones_selfSolveOneTest) {
 
   if (FRINGEJONES_TEST_VERBOSE) {
     cerr << "delay1 results " << p(1,1) << endl;
-    cerr << "delay2 results " << p(4,1) << endl;
+    cerr << "delay2 results " << p(3+1,1) << endl;
     cerr << "rate1 results " << p(2,1) << endl;
-    cerr << "rate2 results " << p(5,1) << endl;
+    cerr << "rate2 results " << p(4+1,1) << endl;
     cerr << "Parameters out: " << p << endl;
   }
   ASSERT_TRUE(allNearAbs(p(1, 1), delay1, 2e-2));
-  ASSERT_TRUE(allNearAbs(p(4, 1), delay2, 2e-2));
+  ASSERT_TRUE(allNearAbs(p(4+1, 1), delay2, 2e-2));
   
   ASSERT_TRUE(allNearAbs(p(2, 1), rate1, 1e-5));
-  ASSERT_TRUE(allNearAbs(p(5, 1), rate2, 1e-5));
+  ASSERT_TRUE(allNearAbs(p(4+2, 1), rate2, 1e-5));
 
   ASSERT_TRUE(FJsol.refant()==0);
   
