485e784e2796b3977c54b251ae93975c6a2106ae
Darrell Schiebel <darrell@schiebel.us>
2019-04-09 14:57:06 -0400

    add copyright

--- src/services/helloworld/casatools/Data/Opt.h
+++ src/services/helloworld/casatools/Data/Opt.h
@@ -1,3 +1,28 @@
+//# Opt.h: simple C++11 alternative to C++17 std::optional
+//# Copyright (C) 2019
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
 #ifndef CASATOOLS_DATA_OPT_H_
 #define CASATOOLS_DATA_OPT_H_
 #include <stdexcept>
