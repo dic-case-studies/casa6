123634790587321cef0f7f8c0c3a0fd8037c02d0
Bob Garwood <bgarwood@nrao.edu>
2019-04-10 12:31:08 -0400

    restore test_imregrid.py to state in master as per developer request, two versions will be maintained

--- gcwrap/python/scripts/tests/test_imregrid.py
+++ gcwrap/python/scripts/tests/test_imregrid.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-from __future__ import print_function
 import os
 import shutil
 import numpy
@@ -26,20 +24,20 @@ def alleqnum(x,num,tolerance=0):
     if len(x.shape)==1:
         for i in range(x.shape[0]):
             if not (abs(x[i]-num) < tolerance):
-                print("x[",i,"]=", x[i])
+                print "x[",i,"]=", x[i]
                 return False
     if len(x.shape)==2:
         for i in range(x.shape[0]):
             for j in range(x.shape[1]):
                 if not (abs(x[i][j]-num) < tolerance):
-                    print("x[",i,"][",j,"]=", x[i][j])
+                    print "x[",i,"][",j,"]=", x[i][j]
                     return False
     if len(x.shape)==3:
         for i in range(x.shape[0]):
             for j in range(x.shape[1]):
                 for k in range(x.shape[2]):
                     if not (abs(x[i][j][k]-num) < tolerance):
-                        print("x[",i,"][",j,"][",k,"]=", x[i][j][k])
+                        print "x[",i,"][",j,"][",k,"]=", x[i][j][k]
                         return False
     if len(x.shape)==4:
         for i in range(x.shape[0]):
@@ -47,7 +45,7 @@ def alleqnum(x,num,tolerance=0):
                 for k in range(x.shape[2]):
                     for l in range(x.shape[3]):
                         if not (abs(x[i][j][k][l]-num) < tolerance):
-                            print("x[",i,"][",j,"][",k,"][",l,"]=", x[i][j][k])
+                            print "x[",i,"][",j,"][",k,"][",l,"]=", x[i][j][k]
                             return False
     if len(x.shape)>4:
         stop('unhandled array shape in alleq')
@@ -56,18 +54,18 @@ def alleqnum(x,num,tolerance=0):
 def test_start(msg):
     global total, current_test
     total += 1
-    print()
-    print(stars + " Test " + msg + " start " + stars)
+    print
+    print stars + " Test " + msg + " start " + stars
     current_test = msg
     
 def test_end(condition, error_msg):
     global total, fail
     status = "OK"
     if not condition:
-        print(error_msg, file=sys.stderr)
+        print >> sys.stderr, error_msg
         fail += 1
         status = "FAIL"
-    print(stars + " Test " + current_test + " " + status + " " + stars)
+    print stars + " Test " + current_test + " " + status + " " + stars
         
 out1 = 'regridded'
 out2 = 'bigger_image'
@@ -107,33 +105,33 @@ class imregrid_test(unittest.TestCase):
         im2.statistics()
         
         rec1 = im1.torecord()
-        print('*************')
-        print(rec1['shape'])
-        print('*************')
+        print '*************'
+        print rec1['shape']
+        print '*************'
         shape = im1.shape()
-        print(shape)
+        print shape
         checked = 0
         for x in range(shape[0]):
             for y in range(shape[1]):
                 p1 = im1.pixelvalue([x, y])
                 p2 = im2.pixelvalue([x, y])
                 if p1['mask'] != p2['mask']:
-                    raise Exception(p1['mask'] + ' != ' + p2['mask'])
-                if p1['value']['value'] != p2['value']['value']: raise Exception(p1['value']['value'] + ' != ' + p2['value']['value'])
-                if p1['value']['unit'] != p2['value']['unit']: raise Exception(p1['value']['unit'] + ' != ' + p2['value']['unit'])
+                    raise Exception, p1['mask'] + ' != ' + p2['mask']
+                if p1['value']['value'] != p2['value']['value']: raise Exception, p1['value']['value'] + ' != ' + p2['value']['value']
+                if p1['value']['unit'] != p2['value']['unit']: raise Exception, p1['value']['unit'] + ' != ' + p2['value']['unit']
                 checked += 3
         
         im2.done()
         
-        print(str(checked) + ' values checked')
+        print str(checked) + ' values checked'
         
         # rescale by factors 3 x 2
         rec1 = im1.torecord()
-        print('*************')
-        print("shape before " + str(rec1['shape']))
-        print('*************')
+        print '*************'
+        print "shape before " + str(rec1['shape'])
+        print '*************'
         rec1['shape'] = numpy.array([3*rec1['shape'][0], 2*rec1['shape'][1]], numpy.int32)
-        print("shape after " + str(rec1['shape']))
+        print "shape after " + str(rec1['shape'])
 
         rec1['coordsys']['coordsys']['direction0']['cdelt'] = [
             rec1['coordsys']['coordsys']['direction0']['cdelt'][0]/3.0,
@@ -141,7 +139,7 @@ class imregrid_test(unittest.TestCase):
         rec1['coordsys']['coordsys']['direction0']['crpix'] = [
             rec1['coordsys']['coordsys']['direction0']['crpix'][0]*3.0,
             rec1['coordsys']['coordsys']['direction0']['crpix'][1]*2.0]
-        print(rec1)
+        print rec1
         
         myia.fromrecord(rec1, out2)
         
@@ -155,16 +153,16 @@ class imregrid_test(unittest.TestCase):
         s1 = imstat(IMAGE)
         s2 = imstat(out1)
         _ia.open(out1)
-        print("out shape " + str(_ia.shape()))
-        print("S1: ", s1)
-        print(" ")
-        print(" ")
-        print("S2: ", s2)
+        print "out shape " + str(_ia.shape())
+        print "S1: ", s1
+        print " "
+        print " "
+        print "S2: ", s2
         
         if s1['maxpos'][0]*3 != s2['maxpos'][0]:
-            raise Exception(str(s1['maxpos'][0]*3) + ' != ' + str(s2['maxpos'][0]))
+            raise Exception, str(s1['maxpos'][0]*3) + ' != ' + str(s2['maxpos'][0])
         if s1['maxpos'][1]*2 != s2['maxpos'][1]:
-            raise Exception(str(s1['maxpos'][1]*2) + ' != ' + str(s2['maxpos'][1]))
+            raise Exception, str(s1['maxpos'][1]*2) + ' != ' + str(s2['maxpos'][1])
         
         
         
@@ -186,9 +184,9 @@ class imregrid_test(unittest.TestCase):
         s1 = imstat(IMAGE)
         s2 = imstat(out1)
         if s1['maxpos'][0]-13 != s2['maxpos'][0]:
-            raise Exception(str(s1['maxpos'][0]-13) + ' != ' + str(s2['maxpos'][0]))
+            raise Exception, str(s1['maxpos'][0]-13) + ' != ' + str(s2['maxpos'][0])
         if s1['maxpos'][1]+1 != s2['maxpos'][1]:
-            raise Exception(str(s1['maxpos'][1]+1) + ' != ' + str(s2['maxpos'][1]))
+            raise Exception, str(s1['maxpos'][1]+1) + ' != ' + str(s2['maxpos'][1])
         
         
         # Shift back to original
@@ -204,20 +202,20 @@ class imregrid_test(unittest.TestCase):
                  output = out4)
         s1 = imstat(IMAGE)
         s2 = imstat(out4)
-        print(s1)
-        print(s2)
+        print s1
+        print s2
         for stat in ['rms', 'medabsdevmed', 'minpos',
                      'min', 'max', 'sum', 'minposf',
                      'median', 'flux', 'sumsq', 'maxposf',
                      'trcf', 'quartile', 'npts', 'maxpos',
                      'mean', 'sigma', 'trc', 'blc', 'blcf']:
             if type(s1[stat]) == type('a string'):
-                print("Checking string", stat, s1[stat])
+                print "Checking string", stat, s1[stat]
                 if s1[stat] != s2[stat]:
                     raise Exception
             else:
                 for i in range(len(s1[stat])):
-                    print("Checking", stat, "[", i, "]", s1[stat][i])
+                    print "Checking", stat, "[", i, "]", s1[stat][i]
                     if s1[stat][i] != s2[stat][i]:
                         # Note:  == comparison of floating point values,
                         # it works right now on this computer but might need to get fixed...
@@ -229,7 +227,7 @@ class imregrid_test(unittest.TestCase):
         rec1 = im1.torecord()
         im1.done()
         for ref in codes:
-            print("Regrid to", ref)
+            print "Regrid to", ref
             if ref not in ['JMEAN', 'JTRUE', 'APP',
                            'BMEAN', 'BTRUE', 'HADEC',
                            'AZEL', 'AZELSW', 'AZELNE',
@@ -479,11 +477,11 @@ class imregrid_test(unittest.TestCase):
                     gcl = cltool()
                     gcl.add(gfit['results']['component0'])
                     grefdir = gcl.getrefdir(0)
-                    print("diff", qa.getvalue(
+                    print "diff", qa.getvalue(
                             qa.convert(
                                 myme.separation(orefdir, grefdir), "arcsec"
                             )
-                        )) 
+                        ) 
                     self.assertTrue(
                         qa.getvalue(
                             qa.convert(
