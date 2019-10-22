5598ddf96124c622af6b977e3e1156aa60e11294
Bob Garwood <bgarwood@nrao.edu>
2019-10-22 14:33:44 -0400

    CAS-12754: edit plotReport to eliminate a deprecation warning in python 3, change is compatible with python 2

--- casa5/gcwrap/python/scripts/imagerhelpers/imager_base.py
+++ casa5/gcwrap/python/scripts/imagerhelpers/imager_base.py
@@ -558,7 +558,7 @@ class PySynthesisImager:
 
         pl.ioff()
 
-        pl.figure(fignum)
+        fig, ax = pl.subplots(nrows=1,ncols=1,num=fignum)
         pl.clf();
         minarr = summ['summaryminor']
         if minarr.size==0:
@@ -615,7 +615,8 @@ class PySynthesisImager:
             pl.xlabel( 'Iteration Count' )
             pl.ylabel( 'Peak Residual (red), Model Flux (blue)' )
 
-            ax = pl.axes()
+            # this is not required (generates an deprecation warning in python 3)
+            # ax = pl.axes()
             box = ax.get_position()
             ax.set_position([box.x0, box.y0, box.width, box.height*0.8])
 
