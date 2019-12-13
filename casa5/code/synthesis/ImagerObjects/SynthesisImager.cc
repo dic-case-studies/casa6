d9d9eed27ff0ac7b9f0711c3670d5bc9540dad99
Kumar Golap <kgolap@nrao.edu>
2019-12-13 16:51:33 -0700

    revert an unnecessary change

--- casa5/code/synthesis/ImagerObjects/SynthesisImager.cc
+++ casa5/code/synthesis/ImagerObjects/SynthesisImager.cc
@@ -857,9 +857,9 @@ bool SynthesisImager::unlockImages()
 	     cs.setSpectralConversion("LSRK");
 	   Vector<Double> pix(4);
            //increase edge part a bit for interpolation
-	   pix[0]=0; pix[1]=0; pix[2]=0; pix[3]=-3.5;
+	   pix[0]=0; pix[1]=0; pix[2]=0; pix[3]=-0.5;
 	   Double freq1=cs.toWorld(pix)[3];
-	   pix[3]=Double(nchannel)+2.5;
+	   pix[3]=Double(nchannel)-0.5;
 	   Double freq2=cs.toWorld(pix)[3];
 	   String units=cs.worldAxisUnits()[3];
 	   if(freq2 < freq1){
