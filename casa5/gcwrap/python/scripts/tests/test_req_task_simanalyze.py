ef11cf30b444b725b56c383d4248dbb214774d81
Andrew McNichols <amcnicho@nrao.edu>
2020-02-20 14:35:01 -0500

    Finish main usage mode tests for test_req_task_simanalyze, add unittest skip to catch known failure modes; for CAS-3669

--- casa5/gcwrap/python/scripts/tests/test_req_task_simanalyze.py
+++ casa5/gcwrap/python/scripts/tests/test_req_task_simanalyze.py
@@ -56,7 +56,6 @@ if CASA6:
                                          (dataroot,
                                           'regression/unittest/clean/refimager/'\
                                           'refim_twopoints_twochan.ms')))
-
 else:
     dataroot = os.environ.get('CASAPATH').split()[0]
     configpath_int = os.path.join(dataroot, 'data/alma/simmos/vla.a.cfg')
@@ -66,7 +65,8 @@ else:
     mspath_sd = os.path.join(dataroot,
                              'data/regression/unittest/clean/refimager/'\
                              'refim_twopoints_twochan.ms')
-
+    
+    
 logpath = casalog.logfile()
 
 ####    Tests     ####
@@ -77,10 +77,15 @@ class simanalyze_main_usage_modes_test(unittest.TestCase):
     def setUpClass(cls):
         '''Called before tests in this class of test cases are run. Executes simobserve to create expected directory structure. Minimally, f"{project}/{project}.{suffix}" where suffix in ['skymodel','newmodel','compskymodel'].'''
 
+        ## create images to serve as skymodel inputs to reference simulations?
+        # gaussian = np.random.multivariate_normal(mean=[0,0], cov=[[1, 0], [0, 100]], size=[128,128])
+        # gaus_img = gaussian[:, :, np.newaxis, :] # add degenerate axis to represent stokes
+        # ia.newimagefromarray('demo.im', gaus_imag, csys, linear=False, overwrite=True, log=True
+
         # create first reference simulation
         int_project = 'sim_interferometric'
         simobserve(project=int_project, 
-                   skymodel=imagepath_int, 
+                   skymodel=imagepath_sd, 
                    complist='', 
                    setpointings=True, 
                    direction=[], 
@@ -99,8 +104,8 @@ class simanalyze_main_usage_modes_test(unittest.TestCase):
                    leakage=0.0, 
                    graphics='none',
                    verbose=False, 
-                   overwrite=False)
-        
+                   overwrite=False)        
+
         # create reference image > 2.5*PB to use for SD sim
         imagepath_sd = 'sd_model.image'
         tclean(vis=mspath_sd, 
@@ -123,7 +128,7 @@ class simanalyze_main_usage_modes_test(unittest.TestCase):
                    complist='', 
                    setpointings=True, 
                    direction=[], 
-                   mapsize=['1deg'], 
+                   mapsize='', 
                    maptype='square', 
                    pointingspacing='',
                    caldirection='',
@@ -142,55 +147,55 @@ class simanalyze_main_usage_modes_test(unittest.TestCase):
                    verbose=False, 
                    overwrite=False)
 
-        # create third reference simulation, with both int and sd
-        # using the ALMA PM antennas for the interferometric case
+        # # create third reference simulation, with both int and sd
+        # # using the ALMA PM antennas for the interferometric case
         both_project = 'sim_both'
-        simobserve(project=both_project, 
-                   skymodel=imagepath_sd, 
-                   complist='', 
-                   setpointings=True, 
-                   direction=[], 
-                   mapsize=['1deg'], 
-                   maptype='square', 
-                   pointingspacing='',
-                   caldirection='',
-                   calflux='1Jy',
-                   obsmode='sd', 
-                   refdate='2020/02/13', 
-                   hourangle='transit', 
-                   totaltime='100s', 
-                   antennalist=configpath_int,
-                   sdantlist=configpath_sd, 
-                   sdant=0,
-                   outframe='LSRK', 
-                   thermalnoise='', 
-                   leakage=0.0, 
-                   graphics='none',
-                   verbose=False, 
-                   overwrite=False)
-        simobserve(project=both_project, 
-                   skymodel=imagepath_sd, 
-                   complist='', 
-                   setpointings=True, 
-                   direction=[], 
-                   mapsize=['1deg'], 
-                   maptype='square', 
-                   pointingspacing='',
-                   caldirection='',
-                   calflux='1Jy',
-                   obsmode='int', 
-                   refdate='2020/02/13', 
-                   hourangle='transit', 
-                   totaltime='100s', 
-                   antennalist=configpath_int,
-                   sdantlist=configpath_sd, 
-                   sdant=0,
-                   outframe='LSRK', 
-                   thermalnoise='', 
-                   leakage=0.0, 
-                   graphics='none',
-                   verbose=False, 
-                   overwrite=False)
+        # simobserve(project=both_project, 
+        #            skymodel=imagepath_sd, 
+        #            complist='', 
+        #            setpointings=True, 
+        #            direction=[], 
+        #            mapsize=['1deg'], 
+        #            maptype='square', 
+        #            pointingspacing='',
+        #            caldirection='',
+        #            calflux='1Jy',
+        #            obsmode='sd', 
+        #            refdate='2020/02/13', 
+        #            hourangle='transit', 
+        #            totaltime='100s', 
+        #            antennalist=configpath_int,
+        #            sdantlist=configpath_sd, 
+        #            sdant=0,
+        #            outframe='LSRK', 
+        #            thermalnoise='', 
+        #            leakage=0.0, 
+        #            graphics='none',
+        #            verbose=False, 
+        #            overwrite=False)
+        # simobserve(project=both_project, 
+        #            skymodel=imagepath_sd, 
+        #            complist='', 
+        #            setpointings=True, 
+        #            direction=[], 
+        #            mapsize=['1deg'], 
+        #            maptype='square', 
+        #            pointingspacing='',
+        #            caldirection='',
+        #            calflux='1Jy',
+        #            obsmode='int', 
+        #            refdate='2020/02/13', 
+        #            hourangle='transit', 
+        #            totaltime='100s', 
+        #            antennalist=configpath_int,
+        #            sdantlist=configpath_sd, 
+        #            sdant=0,
+        #            outframe='LSRK', 
+        #            thermalnoise='', 
+        #            leakage=0.0, 
+        #            graphics='none',
+        #            verbose=False, 
+        #            overwrite=False)
   
     def setUp(self):
         '''Method called to prepare the test fixture.  This is called immediately before calling the test method'''
@@ -295,14 +300,15 @@ class simanalyze_main_usage_modes_test(unittest.TestCase):
         b = True # Expected value
         self.assertEqual(a,b) 
 
-    def test_imaging_True_interferometric_only_analysis_False(self):
+    def test_imaging_True_interferometric_analysis_False(self):
         '''
         test_simanalyze: Not all of the output files will be generated depending on parameter selections
         test_simanalyze: The image parameter determines if an image is produced (False for no .image file)
         test_simanalyze: The analyze parameter turns on or off the creation of analytical images
         '''
-        visname = str(int_project +'/'+ int_project + '.' + 
-                      configpath_int.split('/')[-1][:-3] +'ms')
+        visname_int = str(int_project +'/'+ int_project + '.' + 
+                          configpath_int.split('/')[-1][:-3] +'ms')
+
         simanalyze(project=int_project, 
                    image=True, 
                    vis=visname, 
@@ -336,12 +342,12 @@ class simanalyze_main_usage_modes_test(unittest.TestCase):
                    logfile=logpath)
 
         # Observed value
-        a = (os.path.isdir(visname[:-2]+'image'))
+        a = (os.path.isdir(visname_int[:-2]+'image'))
         # (also [mask, pb, model, residual, sumwt]...)
         b = True # Expected value
         self.assertEqual(a,b) 
 
-    def test_imaging_True_total_power_only_analysis_False(self):
+    def test_imaging_True_single_dish_analysis_False(self):
         '''
         test_simanalyze: Not all of the output files will be generated depending on parameter selections
         test_simanalyze: The image parameter determines if an image is produced (False for no .image file)
@@ -349,8 +355,9 @@ class simanalyze_main_usage_modes_test(unittest.TestCase):
         test_simanalyze: The vis parameter can accept one or more MSs that can be interferometric or single dish data
         test_simanalyze: Modelimage will not be used if the MS is in total power
         '''
-        visname = str(sd_project +'/'+ sd_project + '.' + 
-                      configpath_sd.split('/')[-1][:-3] +'sd.ms')
+        visname_sd = str(sd_project +'/'+ sd_project + '.' + 
+                         configpath_sd.split('/')[-1][:-3] +'sd.ms')
+
         simanalyze(project=sd_project, 
                    image=True, 
                    vis=visname, 
@@ -391,9 +398,16 @@ class simanalyze_main_usage_modes_test(unittest.TestCase):
         b = True # Expected value
         self.assertEqual(a,b) 
 
-    def test_imaging_True_interferometric_and_total_power_analysis_False(self):
-        '''test_simanalyze: Requirement'''
-
+    @unittest.skip("Still need data to generate reference simulation for "\
+                   "single dish + interferometry case")
+    def test_imaging_True_interferometric_and_single_dish_analysis_False(self):
+        '''
+        test_simanalyze: Not all of the output files will be generated depending on parameter selections
+        test_simanalyze: The image parameter determines if an image is produced (False for no .image file)
+        test_simanalyze: The analyze parameter turns on or off the creation of analytical images
+        test_simanalyze: The vis parameter can accept one or more MSs that can be interferometric or single dish data
+        test_simanalyze: Modelimage will not be used if the MS is in total power
+        '''
         simanalyze(project=both_project, 
                    image=True, 
                    vis= 'default', # should grid TP, image IF, feather
@@ -426,63 +440,116 @@ class simanalyze_main_usage_modes_test(unittest.TestCase):
                    dryrun=False, 
                    logfile=logpath)
 
-        # Some Single Test
-        a = 1 # Observed value
-        b = 1 # Expected value
+        visname_both_int = str(both_project +'/'+ both_project + '.' + 
+                               configpath_int.split('/')[-1][:-3] +'ms')
+        visname_both_sd = str(both_project +'/'+ both_project + '.' + 
+                              configpath_sd.split('/')[-1][:-3] +'sd.ms')
+
+        # confirm that both the IF part and SD part generated output
+        a = (os.path.isdir(visname_int[:-2]+'image') and
+             os.path.isdir(both_project+'/'+both_project+'.sd.image') and 
+             os.path.isdir(both_project+'/'+both_project+'.image0') and 
+             os.path.isdir(both_project+'/'+both_project+'.sd.image0.scaled') and 
+             os.path.isdir(both_project+'/'+both_project+'.sd.image0.weight'))
+        b = True # Expected value
         self.assertEqual(a,b) 
 
-    def test_imaging_False_analysis_True_showfidelity_True(self):
+    @unittest.skipUnless(os.path.isdir(int_project+'/'+int_project+'.image'),
+                         "Analysis-only mode presumes the existence of some image data,"\
+                         " such as that generated by test_imaging_True_interferometric*")
+    def test_imaging_False_interferometric_analysis_True_showfidelity_True(self):
         '''
         test_simanalyze: Not all of the output files will be generated depending on parameter selections
         test_simanalyze: The image parameter determines if an image is produced (False for no .image file)
         test_simanalyze: The analyze parameter turns on or off the creation of analytical images
-        test_simanalyze:  The showfidelity parameter displays the fidelity image
+        test_simanalyze: The showfidelity parameter displays the fidelity image
         '''
+        visname_int = str(int_project +'/'+ int_project + '.' + 
+                          configpath_int.split('/')[-1][:-3] +'ms')
+
         simanalyze(project=int_project, 
                    image=False, 
-                   imagename=imagepath_int, 
-                   skymodel=int_project+'/'+int_project+".skymodel", 
+                   # vis=visname, 
+                   # #imagename='', 
+                   # modelimage='', # not the skymodel
+                   # imsize = [],
+                   # imdirection ='',
+                   # cell = '',
+                   # interactive = False, 
+                   # niter = 0,
+                   # threshold = '0.1mJy',
+                   # weighting = 'natural',
+                   # mask = [],
+                   # outertaper = [],
+                   # pbcor = False,
+                   # stokes = 'I', 
+                   # featherimage = '',
                    analyze=True, 
-                   showuv=False,
-                   showpsf=False,
-                   showmodel=False,
-                   showconvolved=False,
-                   showclean=False,
-                   showresidual=False,
-                   showdifference=False,
+                   # showuv=False,
+                   # showpsf=False,
+                   # showmodel=False,
+                   # showconvolved=False,
+                   # showclean=False,
+                   # showresidual=False,
+                   # showdifference=False,
                    showfidelity=True,
                    graphics='none', 
                    verbose=False, 
-                   overwrite=False, 
+                   overwrite=True, 
                    dryrun=False, 
                    logfile=logpath)
 
-        # A fidelity image should have been produced
-        a = os.path.exists() # Observed value
+        # confirm that fidelity image was generated
+        a = os.path.isdir(visname_int[:-2]+'fidelity')
         b = True # Expected value
         self.assertEqual(a,b) 
- 
-    def test_imaging_True_interferometric_only_analysis_True(self):
-        '''test_simanalyze: Requirement'''
-        # Some Single Test
-        a = 1 # Observed value
-        b = 1 # Expected value
-        self.assertEqual(a,b) 
 
-    def test_imaging_True_total_power_only_analysis_True(self):
-        '''test_simanalyze: Requirement'''
-        # Some Single Test
-        a = 1 # Observed value
-        b = 1 # Expected value
-        self.assertEqual(a,b) 
+    @unittest.skipUnless(os.path.isdir(sd_project+'/'+sd_project+'.image'), 
+                         "Analysis-only mode presumes the existence of some image data,"\
+                         " such as that generated by test_imaging_True_single_dish*")
+    def test_imaging_False_single_dish_analysis_True_showfidelity_True(self):
+        '''
+        test_simanalyze: Not all of the output files will be generated depending on parameter selections
+        test_simanalyze: The image parameter determines if an image is produced (False for no .image file)
+        test_simanalyze: The analyze parameter turns on or off the creation of analytical images
+        test_simanalyze: The showfidelity parameter displays the fidelity image
+        '''
+        simanalyze(project=sd_project, 
+                   image=False, 
+                   # vis=visname, 
+                   imagename='', 
+                   # modelimage='', # not the skymodel
+                   # imsize = [],
+                   # imdirection ='',
+                   # cell = '',
+                   # interactive = False, 
+                   # niter = 0,
+                   # threshold = '0.1mJy',
+                   # weighting = 'natural',
+                   # mask = [],
+                   # outertaper = [],
+                   # pbcor = False,
+                   # stokes = 'I', 
+                   # featherimage = '',
+                   analyze=True, 
+                   # showuv=False,
+                   # showpsf=False,
+                   # showmodel=False,
+                   # showconvolved=False,
+                   # showclean=False,
+                   # showresidual=False,
+                   # showdifference=False,
+                   showfidelity=True,
+                   graphics='none', 
+                   verbose=False, 
+                   overwrite=True, 
+                   dryrun=False, 
+                   logfile=logpath)
 
-    def test_imaging_True_interferometric_and_total_power_analysis_True(self):
-        '''test_simanalyze: Requirement'''
-        # Some Single Test
-        a = 1 # Observed value
-        b = 1 # Expected value
+        # confirm that fidelity image was generated
+        a = os.path.isdir(sd_project +'/'+ sd_project + '.' +'sd.fidelity')
+        b = True # Expected value
         self.assertEqual(a,b) 
-
 
 ####    Suite: Required for CASA5     ####
 def suite():
