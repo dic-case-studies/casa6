7b8f03caccdd59d1152b2da1a9ed65f3ad049353
Federico Montesino Pouzols <fpouzols@eso.org>
2020-02-21 20:51:41 +0100

    set flagdataFlagPropagation (time)AveragingTVI opt in flagdata, CAS-12737

--- casa5/code/flagging/Flagging/FlagDataHandler.cc
+++ casa5/code/flagging/Flagging/FlagDataHandler.cc
@@ -1692,6 +1692,9 @@ FlagDataHandler::setTimeAverageIter(bool activated)
         timeAvgOptions_p |= vi::AveragingOptions::AverageModel;
         timeAvgOptions_p |= vi::AveragingOptions::ModelFlagWeightAvgFromWEIGHT;
     }
+
+    // ensure preservation of flags (CAS-12737, CAS-12205, CAS-9985)
+    timeAvgOptions_p |= vi::AveragingOptions::flagdataFlagPropagation;
 }
 
 // ----------------------------------------------------------------------------
