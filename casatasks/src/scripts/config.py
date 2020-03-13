bee5c2278bdd64fff55604a41f23dcb2a6d0b98f
Ville Suoranta <vsuorant@nrao.edu>
2020-03-13 15:13:55 -0400

    Read telemetry toggle from casatasks config instead of .casarc

--- casatasks/src/scripts/config.py
+++ casatasks/src/scripts/config.py
@@ -7,3 +7,7 @@ import os as _os
 import time as _time
 from .private import userconfig as _uc
 logfile = _os.path.realpath(_uc.logfile) if 'logfile' in dir(_uc) else _os.path.join(_os.getcwd( ),'casa-'+_time.strftime("%Y%m%d-%H%M%S", _time.gmtime())+'.log')
+if 'telemetry_enabled' in dir(_uc):
+    telemetry_enabled = _uc.telemetry_enabled
+else:
+    telemetry_enabled=True
