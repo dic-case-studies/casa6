39a880d6c0ae17e54e7d6cc63a4d35725867e207
Ville Suoranta <vsuorant@nrao.edu>
2020-03-16 16:57:07 -0400

    Add task start time to the telemetry message

--- casatasks/src/scripts/task_logging.py
+++ casatasks/src/scripts/task_logging.py
@@ -4,27 +4,29 @@ import casatasks
 
 def start_log( tname, arguments ):
     spaces = ' '*(18-len(tname))
+    start_time = str(_time.now())
     try:
-        if casatasks.telemetry_enabled:
-            casatasks.telemetrylogger.logger.info(str(_time.now()) + ' Begin Task: ' + tname + spaces)
+        if casatasks.config.telemetry_enabled:
+            casatasks.telemetrylogger.logger.info(start_time + ' Begin Task: ' + tname + spaces)
     except:
         pass
     _clog.origin(tname)
     _clog.post( '##########################################' )
     _clog.post( '##### Begin Task: ' + tname + spaces + ' #####' )
     _clog.post( '%s( %s )' % ( tname, ', '.join(arguments) ))
-    return str(_time.now()),
+    return start_time,
 
 def end_log( state, tname, result ):
     spaces = ' '*(18-len(tname))
+    end_time = str(_time.now())
     try:
-        if casatasks.telemetry_enabled:
-            casatasks.telemetrylogger.logger.info(str(_time.now()) + ' End Task: ' + tname + spaces)
+        if casatasks.config.telemetry_enabled:
+            casatasks.telemetrylogger.logger.info(end_time + ' End Task: ' + tname + spaces + 'Start time: ' + state[0])
     except:
         pass
     _clog.origin(tname)
     _clog.post( 'Result %s: %s' % (tname, repr(result)) )
-    _clog.post( 'Task ' + tname + ' complete. Start time: ' + state[0] + ' End time: ' + str(_time.now()) )
+    _clog.post( 'Task ' + tname + ' complete. Start time: ' + state[0] + ' End time: ' + end_time )
     _clog.post( '##### End Task: ' + tname + '  ' + spaces + ' #####' )
     _clog.post( '##########################################' )
     return result
