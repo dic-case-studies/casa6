b778ece9d043d2e1f263e524658ec57bd5a5c609
Sandra Castro <scastro@eso.org>
2020-04-23 09:11:47 +0000

    Forgot the forward slash in /data

--- casatests/stakeholders/test_stk_alma_pipeline_imaging.py
+++ casatests/stakeholders/test_stk_alma_pipeline_imaging.py
@@ -83,7 +83,7 @@ except ImportError:
 
     _ia = iatool()
     def ctsys_resolve(apath):
-        if os.path.exists(os.environ.get('CASAPATH').split()[0] + 'data/casa-data-req'):
+        if os.path.exists(os.environ.get('CASAPATH').split()[0] + '/data/casa-data-req'):
             dataPath = os.path.join(os.environ['CASAPATH'].split()[0],'data/casa-data-req/')
         else:
             dataPath = os.path.join(os.environ['CASAPATH'].split()[0], 'casa-data-req/')
