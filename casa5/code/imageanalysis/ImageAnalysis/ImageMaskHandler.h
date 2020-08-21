8307a19dc0b7144b5b6e9653845b8d8b131e3f0d
Darrell Schiebel <darrell@schiebel.us>
2020-08-21 14:42:44 -0400

    Array headers were leaky (CAS-13054)

--- casa5/code/imageanalysis/ImageAnalysis/ImageMaskHandler.h
+++ casa5/code/imageanalysis/ImageAnalysis/ImageMaskHandler.h
@@ -4,6 +4,7 @@
 #include <casacore/casa/BasicSL/String.h>
 #include <casacore/casa/Arrays/Vector.h>
 #include <casacore/casa/Containers/Record.h>
+#include <casacore/casa/Containers/Block.h>
 #include <imageanalysis/ImageTypedefs.h>
 
 // #include <lattices/Lattices/LatticeExprNode.h>
