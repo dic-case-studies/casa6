5355dda0e654d19dbb1b338add342c3632a9cbfb
Sandra M Castro <scastro@eso.org>
2020-08-28 17:23:51 +0200

    Fixed test cvel and cvel2 due to changes in CAS-13018

--- casatasks/tests/tasks/test_cvel.py
+++ casatasks/tests/tasks/test_cvel.py
@@ -149,31 +149,44 @@ class cvel_test(unittest.TestCase):
     
     def test1(self):
         '''Cvel 1: Testing default - expected error'''
-        myvis = vis_b
-        os.system('ln -sf ' + myvis + ' myinput.ms')
-        passes = False
-        try:
-            rval = cvel()
-            # CASA5 returns False for this expected error
-            passes = not rval
-        except AssertionError:
-            passes = True
-            print("*** Expected error ***")
-        self.assertTrue(passes)
+        if is_CASA6:
+            with self.assertRaises(AssertionError):
+                cvel()
+        else:
+            with self.assertRaises(RuntimeError):
+                cvel()
+#         myvis = vis_b
+#         os.system('ln -sf ' + myvis + ' myinput.ms')
+#         passes = False
+#         try:
+#             rval = cvel()
+#             # CASA5 returns False for this expected error
+#             passes = not rval
+#         except AssertionError:
+#             passes = True
+#             print("*** Expected error ***")
+#         self.assertTrue(passes)
 
     def test2(self):
         '''Cvel 2: Only input vis set - expected error'''
         myvis = vis_b
         os.system('ln -sf ' + myvis + ' myinput.ms')
-        passes = False
-        try:
-            rval = cvel(vis = 'myinput.ms')
-            # CASA5 returns False for this expected error
-            passes = not rval
-        except AssertionError:
-            passes = True
-            print("*** Expected error ***")
-        self.assertTrue(passes)
+        if is_CASA6:
+            with self.assertRaises(AssertionError):
+                cvel(vis = 'myinput.ms')
+        else:
+            with self.assertRaises(AssertionError):
+                cvel(vis = 'myinput.ms')
+        
+#         passes = False
+#         try:
+#             rval = cvel(vis = 'myinput.ms')
+#             # CASA5 returns False for this expected error
+#             passes = not rval
+#         except AssertionError:
+#             passes = True
+#             print("*** Expected error ***")
+#         self.assertTrue(passes)
             
     def test3(self):
         '''Cvel 3: Input and output vis set'''
