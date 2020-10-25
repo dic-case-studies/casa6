8a7cff77e01e1ea59e87534b5263e7c27510431e
César Enrique García Dabó <cgarcia@eso.org>
2020-10-25 10:26:17 +0100

    Set the frequency scope to subchunk at most, not to break current application expectations.

--- casa5/code/msvis/MSVis/VisibilityIteratorImpl2.cc
+++ casa5/code/msvis/MSVis/VisibilityIteratorImpl2.cc
@@ -1616,6 +1616,11 @@ void VisibilityIteratorImpl2::setMetadataScope()
                 freqSelScope_p = timeScope_p;
         }
     }
+
+    // Warning. In order not to break expectations of current applications,
+    // set the frequency scope granularity to subchunk at most.
+    if (freqSelScope_p == RowScope)
+        freqSelScope_p = SubchunkScope;
 }
 
 VisibilityIteratorImpl2::Cache::Cache()
