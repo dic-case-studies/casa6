391d00b36c8e06a7734e53ed4ab5aab5942410aa
Darrell Schiebel <darrell@schiebel.us>
2020-10-28 14:28:48 -0400

    add tec_maps to casatasks.private (CAS-13040)

--- casatasks/setup.py
+++ casatasks/setup.py
@@ -294,6 +294,7 @@ private_scripts = [ 'src/scripts/userconfig.py',
                     'src/scripts/task_logging.py',
                     'src/tasks/task_sdintimaging.py',
                     'src/scripts/sdint_helper.py',
+                    'src/scripts/tec_maps.py',
 ]
 
 private_modules = [ 'src/modules/parallel', 'src/modules/imagerhelpers' ]
