4810ab4a7dd0f745646cf6285b6f4a4fb842225f
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2020-10-28 13:48:02 +0900

    CAS-13160 add practical unit test

--- casatasks/tests/tasks/test_sdatmcor.py
+++ casatasks/tests/tasks/test_sdatmcor.py
@@ -1,7 +1,10 @@
-import unittest
+import glob
 import os
-# import numpy
+import numpy as np
+import shutil
 import sys
+import unittest
+
 from casatasks.private.casa_transition import is_CASA6
 if is_CASA6:
     from casatasks import sdatmcor
@@ -19,6 +22,8 @@ if is_CASA6:
 
     datapath = ctsys.resolve('')
 
+    ctsys_resolve = ctsys.resolve
+
 else:
     from tasks import sdatmcor
     from __main__ import default
@@ -26,6 +31,33 @@ else:
 
     # Define the root for the data files
     datapath = os.environ.get('CASAPATH').split()[0] + ''
+
+    def ctsys_resolve(apath):
+        dataPath = os.path.join(os.environ['CASAPATH'].split()[0], 'data')
+        subdir_hints = ['', 'casa-data-req']
+        for subdir in subdir_hints:
+            path = os.path.join(dataPath, subdir, apath)
+            if os.path.exists(path):
+                return path
+
+
+def smart_remove(name):
+    if os.path.exists(name):
+        if os.path.isdir(name):
+            shutil.rmtree(name)
+        elif os.path.isfile(name):
+            os.remove(name)
+        else:
+            # could be a symlink
+            os.remove(name)
+
+
+def std_with_clip(arr, num_iter=1, factor=3):
+    assert num_iter > 0
+    std = np.abs(arr).max()
+    for i in range(num_iter):
+        std = arr[np.abs(arr) < std * factor].std()
+    return std
 
 #
 # Test-MS
@@ -46,21 +78,35 @@ defWorkMsBasic = "uid___A002_Xe770d7_X320b-t.ms"
 
 
 class test_sdatmcor(unittest.TestCase):
+    datapath = ctsys_resolve('visibilities/almasd')
+    infile = 'X320b_sel.ms'
+    outfile = infile + '.atmcor'
 
-    local_unit_test = True
+    local_unit_test = False
+
+    def __get_temporary_files(self):
+        return glob.glob('_AtmCor_Temp*,ms')
 
     def setUp(self):
-
         default(sdatmcor)
 
         # default Args
-        self.args = {'infile': defInputMs,
-                     'datacolumn': 'float_data'
-                     }
-        
+        self.args = {
+            'infile': self.infile,
+            'datacolumn': 'data',
+            'outfile': self.outfile,
+        }
+
+        smart_remove(self.infile)
+        smart_remove(self.outfile)
+        shutil.copytree(os.path.join(self.datapath, self.infile), self.infile)
+
     def tearDown(self):
         print("tearDown::deleting MSs.")
-
+        smart_remove(self.infile)
+        smart_remove(self.outfile)
+        for tmp in self.__get_temporary_files():
+            smart_remove(tmp)
 
 # private function #
 
@@ -165,17 +211,78 @@ class test_sdatmcor(unittest.TestCase):
 ############################
 
 
-    def test_param1(self):
-        '''sdatmcor::1:: file1  '''
+    # def test_param1(self):
+    #     '''sdatmcor::1:: file1  '''
 
-        infile = 'uid___A002_Xe770d7_X320b-t.ms'
-        outfile = 'sdatmcor-out1.ms'
-        prm = {'debug': 'skipTaskExec',
-               'infile' : infile,
-               'outfile': outfile,
-               'overwrite':  True }
-        # Run Task and check
-        self.assertTrue(self._run_task(prm))
+    #     infile = 'uid___A002_Xe770d7_X320b-t.ms'
+    #     outfile = 'sdatmcor-out1.ms'
+    #     prm = {'debug': 'skipTaskExec',
+    #            'infile' : infile,
+    #            'outfile': outfile,
+    #            'overwrite':  True }
+    #     # Run Task and check
+    #     self.assertTrue(self._run_task(prm))
+
+    def check_result(self):
+        # outfile should exist
+        self.assertTrue(os.path.exists(self.outfile))
+
+        # no temporary files exist
+        self.assertEqual(len(self.__get_temporary_files()), 0)
+
+        # examine averaged spectrum
+        #   - exclude edge channels (4 channels)
+        #   - take average along pol and time axes
+        #   - subtract zero order baseline
+        #   - compute std with clipping (3 iterations => 2 clipping)
+        #   - set threshold to 7.5 times std
+        #   - check if no data exceeds threshold
+        with tbmanager(self.outfile) as tb:
+            stateids = tb.getcol('STATE_ID')
+            data = tb.getcol('DATA').real
+
+        edge = 4
+        data_on = data[:, edge:-edge, stateids == 14]
+        data_mean = data_on.mean(axis=(0, 2))
+        data_sub = data_mean - np.median(data_mean)
+        threshold = 7.5 * std_with_clip(data_sub, num_iter=3)
+        self.assertTrue(np.all(np.abs(data_sub) < threshold))
+
+    def test_sdatmcor_normal(self):
+        '''test normal usage of sdatmcor'''
+        sdatmcor(infile=self.infile, outfile=self.outfile, datacolumn='data')
+        self.check_result()
+
+    def test_sdatmcor_explicit_atmtype(self):
+        '''test specifying atmtype explicitly'''
+        sdatmcor(infile=self.infile, outfile=self.outfile, datacolumn='data', atmtype=2)
+        self.check_result()
+
+    def test_sdatmcor_overwrite(self):
+        '''test overwriting existing outfile'''
+        os.mkdir(self.outfile)
+        self.assertTrue(os.path.exists(self.outfile))
+        sdatmcor(infile=self.infile, outfile=self.outfile, datacolumn='data', overwrite=True)
+        self.check_result()
+
+    def test_sdatmcor_no_overwrite(self):
+        '''test to avoid overwriting existing outfile'''
+        os.mkdir(self.outfile)
+        self.assertTrue(os.path.exists(self.outfile))
+        # TODO: sdatmcor should throw exception
+        self.assertFalse(
+            sdatmcor(infile=self.infile, outfile=self.outfile, datacolumn='data', overwrite=False)
+        )
+
+    def test_sdatmcor_wrong_datacolumn(self):
+        '''test wrong datacolumn'''
+        wrong_colnames = ['corrected', 'float_data']
+        for colname in wrong_colnames:
+            # TODO: sdatmcor should throw exception
+            self.assertFalse(
+                sdatmcor(infile=self.infile, outfile=self.outfile, datacolumn=colname)
+            )
+
 
 def suite():
     return [test_sdatmcor]
@@ -191,6 +298,6 @@ History
              - added 'skipTaskExec'  on debug argument.
 
 17-SEP-2020  Totally simplified, by minimum functions.
-             - This calls the Task with skipTaskExec. 
+             - This calls the Task with skipTaskExec.
                simply called and returns soon.
 """
