2b9d18750878aeff805afc61eeac68c1ded7305e
Darrell Schiebel <darrell@schiebel.us>
2021-01-27 08:57:20 -0500

    update task_msuvbin for python 2/3 (CAS-12401)

--- casa5/gcwrap/python/scripts/task_msuvbin.py
+++ casa5/gcwrap/python/scripts/task_msuvbin.py
@@ -1,15 +1,21 @@
+from __future__ import absolute_import
+
 import os
 import shutil
-from taskinit import casalog
-from casac import casac
 
+from casatasks.private.casa_transition import *
+if is_CASA6:
+    from casatasks import casalog
+else:
+    from taskinit import casalog
+    from casac import casac
 
 def msuvbin(vis=None, field=None, spw=None, taql=None, outvis=None, phasecenter=None, nx=None, ny=None, cell=None,
            ncorr=None, nchan=None, fstart=None, fstep=None, wproject=None, memfrac=None):
     
     casalog.origin('msuvbin ') 
     cmd='msuvbin '
-    pnames=msuvbin.func_code.co_varnames[:msuvbin.func_code.co_argcount]
+    pnames=msuvbin.__init__.__code__.co_varnames[1:]
     for p in pnames:
         pval=eval(p)
         if(not ((pval == '') or (pval ==None))):
@@ -21,7 +27,7 @@ def msuvbin(vis=None, field=None, spw=None, taql=None, outvis=None, phasecenter=
                 cmd=cmd+' '+p+'='+str(pval)
 
 ##############3
-    print 'command ', cmd
+    print('command ', cmd)
 
 ############
     os.system(cmd)
