53d8b5409f78930de2ffb5392a365e0d029904ff
David Mehringer <dmehring@nrao.edu>
2021-03-10 19:16:10 -0500

    Fix pylama linter complaints

--- casa5/gcwrap/python/scripts/tests/test_req_task_phaseshift.py
+++ casa5/gcwrap/python/scripts/tests/test_req_task_phaseshift.py
@@ -27,11 +27,11 @@ try:
     import casatools
     from casatasks import phaseshift, tclean
     CASA6 = True
-    
+
     tb = casatools.table()
     ia = casatools.image()
     ms = casatools.ms()
-    
+
 except ImportError:
     from __main__ import default
     from tasks import *
@@ -52,14 +52,14 @@ if CASA6:
     datapath_ngc = casatools.ctsys.resolve('unittest/phaseshift/ngc7538_ut.ms')
     datapath_nep = casatools.ctsys.resolve('unittest/phaseshift/nep2-shrunk.ms')
     datapath_mms = casatools.ctsys.resolve('unittest/phaseshift/uid___X02_X3d737_X1_01_small.mms')
-    
+
 else:
     datapath = os.environ.get('CASAPATH').split()[0] + '/casatestdata/unittest/phaseshift/refim_twopoints_twochan.ms'
     datapath_Itziar = os.environ.get('CASAPATH').split()[0] + '/casatestdata/unittest/phaseshift/Itziar.ms'
     datapath_ngc = os.environ.get('CASAPATH').split()[0] + '/casatestdata/unittest/phaseshift/ngc7538_ut.ms'
     datapath_nep = os.environ.get('CASAPATH').split()[0] + '/casatestdata/unittest/phaseshift/nep2-shrunk.ms'
     datapath_mms = os.environ.get('CASAPATH').split()[0] + '/casatestdata/unittest/phaseshift/uid___X02_X3d737_X1_01_small.mms'
-    
+
 def change_perms(path):
     os.chmod(path, 0o777)
     for root, dirs, files in os.walk(path):
@@ -234,74 +234,87 @@ class phaseshift_test(unittest.TestCase):
         tb.open(output)
         data_mean = numpy.mean(tb.getcol('DATA'))
         tb.close()
-        
+
         self.assertTrue(numpy.isclose(data_mean, -0.00968202886279957-0.004072808512879953j))
-        
-        
+
     def test_shiftAndCompare(self):
-        ''' Check that changing the phasecenter with phaseshift and reverting with tclean results in the correct flux values at selected pixel locations '''
-        ### Run fixvis to shift the MS phasecenter to a new location.
-        post_vis = ''
+        '''
+            Check that changing the phasecenter with phaseshift and
+            reverting with tclean results in the correct flux values at
+            selected pixel locations
+        '''
+        # Run phaseshift to shift the MS phasecenter to a new location.
         post_vis = 'post_phaseshift.ms'
         os.system('rm -rf ' + post_vis)
-        phaseshift(vis=datacopy, outputvis=post_vis,phasecenter='J2000 19h53m50 40d06m00')
+        phaseshift(
+            vis=datacopy, outputvis=post_vis,
+            phasecenter='J2000 19h53m50 40d06m00'
+        )
 
-        ### (1) Imaging on the original dataset
+        # (1) Imaging on the original dataset
         os.system('rm -rf im2_pre*')
         tclean(vis=datacopy, imagename='im2_pre',
                imsize=2048,cell='5arcsec',niter=0,
                gridder='wproject', wprojplanes=128,pblimit=-0.1, 
                phasecenter='J2000 19h59m28.449 40d44m01.199')
 
-        ### (2) Image the fixvis'd dataset at it's new phasecenter as image center.
-        post_image=''
-        post_image='im2_post_phaseshift'
+        # (2) Image the phaseshifted dataset at it's new phasecenter as
+        # image center.
+        post_image = ''
+        post_image = 'im2_post_phaseshift'
         os.system('rm -rf im2_post_phaseshift*')
 
-        tclean(vis=post_vis,imagename='im2_post_phaseshift',
-               imsize=2048,cell='5arcsec',niter=0,
-               gridder='wproject', wprojplanes=128,pblimit=-0.1 )
-     
-        ### (3) Imaging on fixvis'd dataset, but with the imaging phasecenter set. 
-        ###  If this is working correctly, it should shift back to the same source positions as the previous tclean result.
-        post_image=''
-        post_image='im2_post_phaseshift_tclean_phasecenter'
+        tclean(
+            vis=post_vis, imagename='im2_post_phaseshift',
+            imsize=2048, cell='5arcsec', niter=0,
+            gridder='wproject', wprojplanes=128, pblimit=-0.1
+        )
+
+        # (3) Imaging on fixvis'd dataset, but with the imaging phasecenter
+        # set. If this is working correctly, it should shift back to the same
+        # source positions as the previous tclean result.
+        post_image = 'im2_post_phaseshift_tclean_phasecenter'
         os.system('rm -rf im2_post_phaseshift_tclean_phasecenter*')
 
-        tclean(vis=post_vis,imagename=post_image,
-               imsize=2048,cell='5arcsec',niter=0, 
-               gridder='wproject', wprojplanes=128,pblimit=-0.1, 
-               phasecenter='J2000 19h59m28.449 40d44m01.199' )
+        tclean(
+            vis=post_vis, imagename=post_image,
+            imsize=2048, cell='5arcsec', niter=0, gridder='wproject',
+            wprojplanes=128, pblimit=-0.1,
+            phasecenter='J2000 19h59m28.449 40d44m01.199' 
+        )
 
-        ### In the above 3 images, (1) has the correct locations.  
-        ### Both (2) and (3) show the offset error when viewed in world coordinates. Open in the viewer as an image stack, and step through.
-        ###  For comparisons with (1), pick the result from (3) because when this works correctly the sources should appear at the same
-        ###  pixel location as in (1).  This test is encoded below. 
+        # In the above 3 images, (1) has the correct locations.
+        # Both (2) and (3) show the offset error when viewed in world
+        # coordinates. Open in the viewer as an image stack, and step
+        # through.
+        # For comparisons with (1), pick the result from (3) because when
+        # this works correctly the sources should appear at the same
+        # pixel location as in (1).  This test is encoded below.
 
         ia.open('im2_pre.image')
-        src1_pre = ia.pixelvalue([ 1024, 1024, 0, 0 ] )['value']
-        src2_pre = ia.pixelvalue([ 1132, 1168, 0, 0 ] )['value']
+        src1_pre = ia.pixelvalue([1024, 1024, 0, 0] )['value']
+        src2_pre = ia.pixelvalue([1132, 1168, 0, 0] )['value']
         ia.close()
         ia.open(post_image+'.image')
-        src1_post = ia.pixelvalue([ 1024, 1024, 0, 0 ] )['value']
-        src2_post = ia.pixelvalue([ 1132, 1168, 0, 0 ] )['value']
+        src1_post = ia.pixelvalue([1024, 1024, 0, 0] )['value']
+        src2_post = ia.pixelvalue([1132, 1168, 0, 0] )['value']
         ia.close()
 
         print("Image value at source locations")
-        print("Original MS : "+str(src1_pre) + " and " + str(src2_pre) )
-        print("Fixvis'd MS : "+str(src1_post) + " and " + str(src2_post) )
-        
+        print("Original MS : "+str(src1_pre) + " and " + str(src2_pre))
+        print("Fixvis'd MS : "+str(src1_post) + " and " + str(src2_post))
+
         os.system('rm -rf im2_pre*')
         os.system('rm -rf im2_post_phaseshift*')
         os.system('rm -rf im2_post_phaseshift_tclean_phasecenter*')
-        
+
         self.assertTrue(numpy.isclose(src1_pre['value'], src1_post['value'], rtol=0.01))
         self.assertTrue(numpy.isclose(src2_pre['value'], src2_post['value'], rtol=0.01))
-        
-        
-        
+
+
 def suite():
     return[phaseshift_test]
+
 
 if __name__ == '__main__':
     unittest.main()
