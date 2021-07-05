1839c96a1c9cc4e77b64d60ce54b8dd6ad2a4d03
Darrell Schiebel <darrell@schiebel.us>
2021-07-05 11:54:26 -0600

    clean up splatalog db search (CAS-13460)

--- casa5/code/spectrallines/Splatalogue/SearcherFactory.cc
+++ casa5/code/spectrallines/Splatalogue/SearcherFactory.cc
@@ -32,34 +32,36 @@ using namespace std;
 using namespace casacore;
 namespace casa {
 
-String SearcherFactory::getLocation( bool local ){
+String SearcherFactory::getLocation( bool local ) {
 	String defaultDatabasePath;
 	if ( local ){
 
 		//Note:: When the SQLite code (and database get put into its
 		//own library, finding the location of the database should be
 		//removed from here and put into the DatabaseConnector.
-        const string splatable = "ephemerides/SplatDefault.tbl";
-        Bool foundDatabase = false;
-        string resolvepath;
-        resolvepath = casatools::get_state( ).resolve(splatable);
-        if ( resolvepath != splatable ) {
-            foundDatabase = true;
-            defaultDatabasePath = resolvepath;
-        } else {
-            foundDatabase = Aipsrc::find(defaultDatabasePath, "user.ephemerides.SplatDefault.tbl");
-            if( !foundDatabase ){
-                foundDatabase = Aipsrc::findDir(defaultDatabasePath, "data/" + splatable);
-            }
-        }
+		const string splatdb = "ephemerides/splatalogue.db";
+		Bool foundDatabase = false;
+		string resolvepath;
+		resolvepath = casatools::get_state( ).resolve(splatdb);
+		if ( resolvepath != splatdb ) {
+			foundDatabase = true;
+			defaultDatabasePath = resolvepath;
+		} else {
+			const string splatable = "ephemerides/SplatDefault.tbl";
+			foundDatabase = Aipsrc::find(defaultDatabasePath, "user.ephemerides.SplatDefault.tbl");
+			if( !foundDatabase ){
+				foundDatabase = Aipsrc::findDir(defaultDatabasePath, "data/" + splatable);
+			}
 
-		if ( foundDatabase ) {
-			const String tableName = "SplatDefault.tbl";
-			int index = defaultDatabasePath.find(tableName, 0);
-			int tableNameSize = tableName.length();
-			defaultDatabasePath.replace(index, tableNameSize, "splatalogue.db");
+			if ( foundDatabase ) {
+				const String tableName = "SplatDefault.tbl";
+				int index = defaultDatabasePath.find(tableName, 0);
+				int tableNameSize = tableName.length();
+				defaultDatabasePath.replace(index, tableNameSize, "splatalogue.db");
+			}
 		}
 	}
+
 	static bool logged = false;
 	if ( logged == false ) {
 		logged = true;
