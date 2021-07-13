891715ddf8aac28510db3b8470d19acbccd74e95
Yohei Hayashi <yohei.hayashi@nao.ac.jp>
2021-07-13 14:20:27 +0900

    Adjust white spaces.

--- casatasks/src/tasks/task_gencal.py
+++ casatasks/src/tasks/task_gencal.py
@@ -11,7 +11,7 @@ if is_CASA6:
     from casatools import calibrater
     from . import correct_ant_posns as getantposns
 
-    _cb = calibrater( )
+    _cb = calibrater()
 else:
     import correct_ant_posns as getantposns
     from taskinit import *
@@ -19,49 +19,49 @@ else:
     (_cb,) = gentools(['cb'])
 
 
-def gencal(vis=None,caltable=None,caltype=None,infile=None,
-        spw=None,antenna=None,pol=None,
-        parameter=None,uniform=None):
+def gencal(vis=None, caltable=None, caltype=None, infile=None,
+           spw=None, antenna=None, pol=None,
+           parameter=None, uniform=None):
 
     """ Externally specify calibration solutions af various types
     """
-
-    #Python script
+   
+    # Python script
     try:
 
-        if ((type(vis)==str) & (os.path.exists(vis))):
+        if ((type(vis) == str) & (os.path.exists(vis))):
             # don't need scr col for this
-            _cb.open(filename=vis,compress=False,addcorr=False,addmodel=False) 
+            _cb.open(filename=vis, compress=False, addcorr=False, addmodel=False) 
         else:
             raise ValueError('Visibility data set not found - please verify the name')
 
-        if (caltable==''):
+        if (caltable == ''):
             raise ValueError('A caltable name must be specified')
 
-        if caltype=='tecim' and not (type(infile)==str and os.path.exists(infile)):
+        if caltype == 'tecim' and not (type(infile) == str and os.path.exists(infile)):
             raise ValueError('An existing tec map must be specified in infile')
 
         # call a Python function to retreive ant position offsets automatically (currently EVLA only)
-        if (caltype=='antpos' and antenna==''):
+        if (caltype == 'antpos' and antenna == ''):
             casalog.post(" Determine antenna position offsets from the baseline correction database")
             # correct_ant_posns returns a list , [return_code, antennas, offsets]
-            antenna_offsets=getantposns.correct_ant_posns(vis,False)
-            if ((len(antenna_offsets)==3) and
-                    (int(antenna_offsets[0])==0) and
-                    (len(antenna_offsets[1])>0) ) :
+            antenna_offsets = getantposns.correct_ant_posns(vis, False)
+            if ((len(antenna_offsets) == 3) and
+                    (int(antenna_offsets[0]) == 0) and
+                    (len(antenna_offsets[1]) > 0)):
                 antenna = antenna_offsets[1]
                 parameter = antenna_offsets[2]
             else:
-                #raise Exception, 'No offsets found. No caltable created.'
-                warnings.simplefilter('error',UserWarning)
+                # raise Exception, 'No offsets found. No caltable created.'
+                warnings.simplefilter('error', UserWarning)
                 warnings.warn('No offsets found. No caltable created.')
 
-        _cb.specifycal(caltable=caltable,time="",spw=spw,antenna=antenna,pol=pol,
-                caltype=caltype,parameter=parameter,infile=infile,
-                uniform=uniform)
+        _cb.specifycal(caltable=caltable, time="", spw=spw, antenna=antenna, pol=pol,
+                       caltype=caltype, parameter=parameter, infile=infile,
+                       uniform=uniform)
 
-        #_cb.close()
-
+        # _cb.close()
+   
     except UserWarning as instance:
         casalog.post('*** UserWarning *** %s' % instance, 'WARN')
 
