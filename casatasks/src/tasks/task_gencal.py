faa8d07e399309385ebbb1712ef9c3c2cfbffad4
Yohei Hayashi <yohei.hayashi@nao.ac.jp>
2021-07-15 11:08:24 +0900

    Gather each caltype logics. Add specifycal for jyperk. Refs #2039

--- casatasks/src/tasks/task_gencal.py
+++ casatasks/src/tasks/task_gencal.py
@@ -55,79 +55,51 @@ def gencal(vis=None, caltable=None, caltype=None, endpoint='asdm', infile=None,
     if not os.path.exists(vis):
         raise ValueError('Visibility data set not found - please verify the name')
 
-    if caltype == 'tecim':
-        __gencal_for_tecim()
-    elif caltype == 'antpos':
-        __gencal_for_antpos()
-    elif caltype == 'jyperk':
-        __gencal_for_jyperk()
 
-
-def __gencal_for_tecim(vis, caltable=None, spw=None, antenna=None, pol=None,
-                        parameter=None, infile=None, uniform=None):
-    try:
-        _cb.open(filename=vis, compress=False, addcorr=False, addmodel=False)
-
-        _cb.specifycal(caltable=caltable, time="", spw=spw, antenna=antenna, pol=pol,
-                       caltype='tecim', parameter=parameter, infile=infile,
-                       uniform=uniform)
-
-    except UserWarning as instance:
-        casalog.post('*** UserWarning *** %s' % instance, 'WARN')
-
-    finally:
-        _cb.close()
-
-def __gencal_for_antpos(vis, caltable=None, spw=None, antenna=None, pol=None,
-                        parameter=None, infile=None, uniform=None):
     try:
         _cb.open(filename=vis, compress=False, addcorr=False, addmodel=False)
 
         # call a Python function to retreive ant position offsets automatically (currently EVLA only)
-        if antenna == '':
-            casalog.post(" Determine antenna position offsets from the baseline correction database")
-            # correct_ant_posns returns a list , [return_code, antennas, offsets]
-            antenna_offsets = getantposns.correct_ant_posns(vis, False)
-            if ((len(antenna_offsets) == 3) and
-                    (int(antenna_offsets[0]) == 0) and
-                    (len(antenna_offsets[1]) > 0)):
-                antenna = antenna_offsets[1]
-                parameter = antenna_offsets[2]
-            else:
-                # raise Exception, 'No offsets found. No caltable created.'
-                warnings.simplefilter('error', UserWarning)
-                warnings.warn('No offsets found. No caltable created.')
-
-        _cb.specifycal(caltable=caltable, time="", spw=spw, antenna=antenna, pol=pol,
-                       caltype='antpos', parameter=parameter, infile=infile,
-                       uniform=uniform)
-
-    except UserWarning as instance:
-        casalog.post('*** UserWarning *** %s' % instance, 'WARN')
-
-    finally:
-        _cb.close()
-
-def __gencal_for_jyperk():
-    try:
-        _cb.open(filename=vis, compress=False, addcorr=False, addmodel=False)
+        if (caltype == 'antpos' and antenna == ''):
+            antenna, parameter = __complete_antpos(vis)
 
         if caltype == 'jyperk':
-            if not infile is None:
-                f = JyPerKReader4File(infile)
-                factors = f.get()
-
-            elif infile is None:
-                factors = gen_factor_via_web_api(vis, endpoint=endpoint, 
-                                                timeout=timeout, retry=retry, 
-                                                retry_wait_time=retry_wait_time)
-
-        _cb.specifycal(caltable=caltable, time="", spw=spw, antenna=antenna, pol=pol,
-                       caltype=caltype, parameter=parameter, infile=infile,
-                       uniform=uniform)
+            for selection, param in __gen_specifycal_input(factors):
+                _cb.specifycal(caltable='amp', time='', spw=selection.spw, 
+                               antenna=selection.antenna, pol=selection.pol, 
+                               parameter=param, infile='', uniform=uniform)
+        else:
+            _cb.specifycal(caltable=caltable, time="", spw=spw, antenna=antenna, pol=pol,
+                           caltype='antpos', parameter=parameter, infile=infile,
+                           uniform=uniform)
 
     except UserWarning as instance:
         casalog.post('*** UserWarning *** %s' % instance, 'WARN')
 
     finally:
         _cb.close()
+
+def __complete_antpos(vis):
+    casalog.post(" Determine antenna position offsets from the baseline correction database")
+    # correct_ant_posns returns a list , [return_code, antennas, offsets]
+    antenna_offsets = getantposns.correct_ant_posns(vis, False)
+    if ((len(antenna_offsets) == 3) and
+            (int(antenna_offsets[0]) == 0) and
+            (len(antenna_offsets[1]) > 0)):
+        antenna = antenna_offsets[1]
+        parameter = antenna_offsets[2]
+        return antenna, parameter
+    else:
+        # raise Exception, 'No offsets found. No caltable created.'
+        warnings.simplefilter('error', UserWarning)
+        warnings.warn('No offsets found. No caltable created.')
+
+def __gen_specifycal_input(vis=None, infile=None, timeout=180, retry=3, retry_wait_time=5):
+    if not infile is None:
+        f = JyPerKReader4File(infile)
+        factors = f.get()
+
+    elif infile is None:
+        factors = gen_factor_via_web_api(vis, endpoint=endpoint, 
+                                        timeout=timeout, retry=retry, 
+                                        retry_wait_time=retry_wait_time)
