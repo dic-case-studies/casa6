c5d5b2f4b41e7784899462950621eef0b7002ab2
Yohei Hayashi <yohei.hayashi@nao.ac.jp>
2021-08-19 13:07:09 +0900

    introduce factory pattern into task_gencal.py

--- casatasks/src/tasks/task_gencal.py
+++ casatasks/src/tasks/task_gencal.py
@@ -62,63 +62,122 @@ def gencal(vis=None, caltable=None, caltype=None, infile='None',
     if not ((type(vis)==str) and (os.path.exists(vis))):
         raise ValueError('Visibility data set not found - please verify the name')
 
-    #Python script
-    try:
-        # don't need scr col for this
-        _cb.open(filename=vis,compress=False,addcorr=False,addmodel=False)  
+    if not caltype in ['antpos', 'jyperk']:
+        gencal_type = 'general'
+    else:
+        gencal_type = caltype
 
-        # call a Python function to retreive ant position offsets automatically (currently EVLA only)
-        if (caltype=='antpos' and antenna==''):
-            casalog.post(" Determine antenna position offsets from the baseline correction database")
-            # correct_ant_posns returns a list , [return_code, antennas, offsets]
-            antenna_offsets=getantposns.correct_ant_posns(vis,False)
-            if ((len(antenna_offsets)==3) and
-                (int(antenna_offsets[0])==0) and
-                (len(antenna_offsets[1])>0) ) :
-                    antenna = antenna_offsets[1]
-                    parameter = antenna_offsets[2] 
-            else:
-                #raise Exception, 'No offsets found. No caltable created.'
-                warnings.simplefilter('error',UserWarning)
-                warnings.warn('No offsets found. No caltable created.')
+    gencal = __gencal_factory[caltype]
+    gencal.gencal(vis=vis, caltable=caltable, caltype=caltype, infile=infile,
+                  endpoint=endpoint, timeout=timeout, retry=retry, retry_wait_time=retry_wait_time,
+                  spw=spw, antenna=antenna, pol=pol, parameter=parameter, uniform=uniform)
 
-        if caltype == 'jyperk':
-            for selection, param in __gen_specifycal_input(vis=vis, spw=spw,
-                                                           endpoint=endpoint, infile=infile,
-                                                           timeout=timeout, retry=retry,
-                                                           retry_wait_time=retry_wait_time):
-                _cb.specifycal(caltable='amp', time='', spw=selection['spw'],
-                               antenna=selection['antenna'], pol=selection['pol'],
-                               parameter=param, infile='', uniform=uniform)
-        else:
+
+__gencal_factory = {
+    'general': GeneralGencal,
+    'antpos': AntposGencal,
+    'jyperk': JyperkGencal,
+}
+
+
+class GeneralGencal():
+    @classmethod
+    def gencal(cls, vis=None, caltable=None, caltype=None, infile='None',
+               endpoint='asdm', timeout=180, retry=3, retry_wait_time=5,
+               spw=None, antenna=None, pol=None,
+               parameter=None, uniform=None):
+        try:
+            # don't need scr col for this
+            _cb.open(filename=vis,compress=False,addcorr=False,addmodel=False)  
             _cb.specifycal(caltable=caltable, time='', spw=spw, antenna=antenna, pol=pol,
                             caltype=caltype, parameter=parameter, infile=infile,
                             uniform=uniform)
        
-    except UserWarning as instance:
-        casalog.post('*** UserWarning *** %s' % instance, 'WARN')
+        except UserWarning as instance:
+            casalog.post('*** UserWarning *** %s' % instance, 'WARN')
 
-    finally:
-        _cb.close()
+        finally:
+            _cb.close()
 
 
-def __gen_specifycal_input(vis=None, spw='*',
-                           endpoint='asdm', infile=None,
-                           timeout=180, retry=3, retry_wait_time=5):
-    if not infile is None:
-        f = JyPerKReader4File(infile)
-        factors = f.get()
+class AntoposGencal():
+    @classmethod
+    def gencal(cls, vis=None, caltable=None, caltype=None, infile='None',
+               endpoint='asdm', timeout=180, retry=3, retry_wait_time=5,
+               spw=None, antenna=None, pol=None,
+               parameter=None, uniform=None):
+        try:
+            # don't need scr col for this
+            _cb.open(filename=vis,compress=False,addcorr=False,addmodel=False)  
 
-    elif infile is None:
-        factors = gen_factor_via_web_api(vis, spw=spw,
-                                         endpoint=endpoint, 
-                                         timeout=timeout, retry=retry, 
-                                         retry_wait_time=retry_wait_time)
+            # call a Python function to retreive ant position offsets automatically (currently EVLA only)
+            if antenna=='':
+                casalog.post(" Determine antenna position offsets from the baseline correction database")
+                # correct_ant_posns returns a list , [return_code, antennas, offsets]
+                antenna_offsets=getantposns.correct_ant_posns(vis,False)
+                if ((len(antenna_offsets)==3) and
+                    (int(antenna_offsets[0])==0) and
+                    (len(antenna_offsets[1])>0) ) :
+                        antenna = antenna_offsets[1]
+                        parameter = antenna_offsets[2] 
+                else:
+                    #raise Exception, 'No offsets found. No caltable created.'
+                    warnings.simplefilter('error',UserWarning)
+                    warnings.warn('No offsets found. No caltable created.')
 
-    for factor in factors:
-        selection = {}
-        selection['antenna'] = factor[1]
-        selection['spw'] = factor[2]
-        selection['pol'] = factor[3]
+            _cb.specifycal(caltable=caltable, time='', spw=spw, antenna=antenna, pol=pol,
+                            caltype=caltype, parameter=parameter, infile=infile,
+                            uniform=uniform)
         
-        yield selection, 1/np.sqrt(float(factor[4]))
+        except UserWarning as instance:
+            casalog.post('*** UserWarning *** %s' % instance, 'WARN')
+
+        finally:
+            _cb.close()
+
+
+class JyperkGencal():
+    @classmethod
+    def gencal(cls, vis=None, caltable=None, caltype=None, infile='None',
+               endpoint='asdm', timeout=180, retry=3, retry_wait_time=5,
+               spw=None, antenna=None, pol=None,
+               parameter=None, uniform=None):
+        try:
+            # don't need scr col for this
+            _cb.open(filename=vis,compress=False,addcorr=False,addmodel=False)  
+
+            for selection, param in JyperkGencal.__gen_specifycal_input(vis=vis, spw=spw,
+                                                        endpoint=endpoint, infile=infile,
+                                                        timeout=timeout, retry=retry,
+                                                        retry_wait_time=retry_wait_time):
+                _cb.specifycal(caltable='amp', time='', spw=selection['spw'],
+                            antenna=selection['antenna'], pol=selection['pol'],
+                            parameter=param, infile='', uniform=uniform)
+       
+        except UserWarning as instance:
+            casalog.post('*** UserWarning *** %s' % instance, 'WARN')
+
+        finally:
+            _cb.close()
+
+    @classmethod
+    def __gen_specifycal_input(cls, vis=None, spw='*',
+                            endpoint='asdm', infile=None,
+                            timeout=180, retry=3, retry_wait_time=5):
+        if not infile is None:
+            f = JyPerKReader4File(infile)
+            factors = f.get()
+
+        elif infile is None:
+            factors = gen_factor_via_web_api(vis, spw=spw,
+                                            endpoint=endpoint, 
+                                            timeout=timeout, retry=retry, 
+                                            retry_wait_time=retry_wait_time)
+
+        for factor in factors:
+            selection = {}
+            selection['antenna'] = factor[1]
+            selection['spw'] = factor[2]
+            selection['pol'] = factor[3]
+            
+            yield selection, 1/np.sqrt(float(factor[4]))
