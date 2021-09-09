6accb255f70fbb0f18f3d263d61afd054948f3c6
Yohei Hayashi <yohei.hayashi@nao.ac.jp>
2021-09-09 12:42:48 +0900

    add a test case which caltype is jyperk and endpoint is asdm.

--- casatasks/tests/tasks/test_gencal.py
+++ casatasks/tests/tasks/test_gencal.py
@@ -574,6 +574,24 @@ class TestJyPerK(unittest.TestCase):
             tb.close()
         return paramlist[0,0], paramlist[1,0]
 
+    def test_jyperk_gencal_for_asdm_web_api(self):
+        """Test to check that the factors in the csv file are applied to the caltable.
+
+        The following arguments are required for this test.
+        * caltype='jyperk'
+        * infile
+        """
+        gencal(vis=self.vis,
+               caltable=self.caltable,
+               caltype='jyperk',
+               endpoint='asdm'
+               uniform=False)
+
+        self.assertTrue(os.path.exists(self.caltable))
+
+        reference_caltable = os.path.join(datapath, 'caltable_for_jyperk_with_asdm_web_api.cal')
+        self.assertTrue(th.compTables(self.caltable, reference_caltable, ['WEIGHT']))
+
     def test_jyperk_gencal_for_factor_file(self):
         """Test to check that the factors in the csv file are applied to the caltable.
 
