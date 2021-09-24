80a326a95df8e2fc84f52a63cf20cfe6a886f902
Yohei Hayashi <yohei.hayashi@nao.ac.jp>
2021-09-24 12:02:43 +0900

    update TestJyPerKReader4File(). fix some bugs. move all logics in setUp and tearDown to setUpClass and tearDownClass. change a logic to make working directory and generate some files in the working directory.

--- casatasks/tests/scripts/test_jyperk.py
+++ casatasks/tests/scripts/test_jyperk.py
@@ -1,17 +1,45 @@
 import os
 import shutil
 import unittest
-from unitetst import mock, MagicMock, patch
 
 from casatasks.private import jyperk
+from casatasks.private.casa_transition import is_CASA6
+
+if is_CASA6:
+    from casatools import ctsys
 
 
 class TestJyPerKReader4File(unittest.TestCase):
-    def setUp(self):
-        self.test_dir = 'test_jyperk'
+    """test TestJyPerKReader class.
+    """
+    working_directory = 'working_directory_for_jyperk'
+    jyperk_factor_path = 'jyperk_factor.csv'
 
-        os.mkdir(self.test_dir)
-        self.jyperk_factor_path = os.path.join(self.test_dir, 'jyperk_factor.csv')
+    @classmethod
+    def setUpClass(cls):
+        cls.casa_cwd_path = os.getcwd()
+
+        if os.path.exists(cls.working_directory):
+            shutil.rmtree(cls.working_directory)
+
+        os.mkdir(cls.working_directory)
+        os.chdir(cls.working_directory)
+
+        cls._generate_jyperk_factor_csv()
+
+        ms_datapath = ctsys.resolve('measurementset/almasd')
+
+    @classmethod
+    def tearDownClass(cls):
+        os.chdir(cls.casa_cwd_path)
+        shutil.rmtree(cls.working_directory)
+
+    def _delete_dir(self, path):
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    @classmethod
+    def _generate_jyperk_factor_csv(cls):
         jyperk_factor_content = """MS,Antenna,Spwid,Polarization,Factor
 uid___A002_X85c183_X36f.ms,DA61,17,I,51.890035198
 uid___A002_X85c183_X36f.ms,PM03,17,I,51.890035198
@@ -23,11 +51,8 @@ uid___A002_X8602fa_X2ab.ms,PM02,17,I,51.4634859397
 uid___A002_X8602fa_X2ab.ms,PM03,17,I,51.4634859397
 uid___A002_X8602fa_X2ab.ms,PM04,17,I,51.4634859397"""
 
-        with open(self.jyperk_factor_path, 'w') as fp:
+        with open(cls.jyperk_factor_path, 'w') as fp:
             fp.write(jyperk_factor_content)
-
-    def tearDown(self):
-        shutil.rmtree(self.test_dir)
         
     def test_get(self):
         jyperk_factor_list = [['uid___A002_X85c183_X36f.ms', 'DA61', '17', 'I', '51.890035198'],
