222ab565b1b10f96d9134f74fcf7e26aaab7c79a
kazuhiko.shimada <kazuhiko.shimada@nao.ac.jp>
2021-12-08 13:49:09 +0900

    CAS-13520: cleaned up functions in this source

--- casatasks/src/private/callabletask.py
+++ casatasks/src/private/callabletask.py
@@ -1,25 +1,14 @@
 import functools
-import re
-
 from casatasks import casalog
-import traceback
 
 
 def callabletask_decorator(func):
     """
     This is a decorator function for a task calls other tasks.
-    Currently the decorator does:
 
-       1) if it get the parameter '__taskcaller', read it and set origin to the logger.
-        otherwise it reads the function name and set origin to the logger.
-       2) handle exception
-
-    So, you don't need to set origin in the task any more.
-    Also, you don't need to write anything about error
-    handling in the task. If you have something to do
-    at the end of the task execution, those should be
-    written in the destructor of worker class, not in
-    the 'finally' block.
+    if it get the parameter '__taskcaller', read it and set origin to the casalog.
+    otherwise it reads the function name and set origin to the logger.
+    So you don't need to set origin in the task any more.
     """
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -32,36 +21,11 @@ def callabletask_decorator(func):
         else:
             casalog.origin(caller)
 
-        retval = None
-        # Any errors are handled outside the task.
-        # however, the implementation below is effectively
-        # equivalent to handling it inside the task.
-        try:
-            # execute task
-            retval = func(*args, **kwargs)
-        except Exception as e:
-            traceback_info = __format_trace(traceback.format_exc())
-            casalog.post(traceback_info, 'SEVERE')
-            casalog.post(str(e), 'ERROR')
-            raise
-        finally:
-            if caller != func.__name__:
-                kwargs['__taskcaller'] = caller
+        retval = func(*args, **kwargs)
+
+        if caller != func.__name__:
+            kwargs['__taskcaller'] = caller
+
         return retval
+
     return wrapper
-
-
-def __format_trace(s):
-    wexists = True
-    regex = '.*callabletask\.py.*in wrapper.*'
-    retval = s
-    while wexists:
-        ss = retval.split('\n')
-        wexists = False
-        for i in range(len(ss)):
-            if re.match(regex, ss[i]):
-                ss = ss[:i] + ss[i + 2:]
-                wexists = True
-                break
-        retval = '\n'.join(ss)
-    return retval
