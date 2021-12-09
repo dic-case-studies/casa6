f369365762be145917ef606775a976fa929def61
R. V. Urvashi <rurvashi@aoc.nrao.edu>
2021-12-09 10:42:43 -0700

    CAS-13530 : Re-enabled new test #3, with float user-specified beam patterns. Previous tests show that tests #1 and #2 are ok and do not cause the test suite to fail.

--- casatasks/tests/tasks/test_tclean.py
+++ casatasks/tests/tasks/test_tclean.py
@@ -4191,7 +4191,7 @@ class test_hetarray_imaging(testref_base):
           self.assertTrue(self.check_final(pstr=report1+report2))
 
 
-     @unittest.skipIf(is_CASA6, "Skip temporarily - to debug test failures in CAS-13530")
+     #@unittest.skipIf(is_CASA6, "Skip temporarily - to debug test failures in CAS-13530")
      def test_het_antenna_mosaic_cube_vptable_float(self):
           '''
           This dataset has two groups of antennas (18m and 6m) with cross baselines. 
