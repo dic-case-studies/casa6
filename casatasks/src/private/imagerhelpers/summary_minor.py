6e1b39d65405c8d258413cd09e8d4327c61fea16
Benjamin Bean <bbean@nrao.edu>
2022-01-21 14:36:16 -0700

    CAS-6692 change all references to polarity to stokes, don't include any of multifieldId/chan/stok/deconId as summaryKeys

--- casatasks/src/private/imagerhelpers/summary_minor.py
+++ casatasks/src/private/imagerhelpers/summary_minor.py
@@ -2,7 +2,7 @@ import copy
 import numpy as np
 import os
 
-from casatasks import casalog
+# from casatasks import casalog
 
 class SummaryMinor:
     """Gathers the information together from the tclean return value in a way that makes it easier to query for exactly what you want.
@@ -11,10 +11,10 @@ class SummaryMinor:
         {
             multi-field id: {
                 "chans": [chan0, chan1, ...],
-                "pols":  [pol0, pol1, ...],
+                "stoks": [stok0, stok1, ...],
                 "ncycs": number of major/minor cycles
                 channel id: {
-                    polarity id: {
+                    stokes id: {
                         summary key: {
                             cycle: value
                         }
@@ -26,12 +26,12 @@ class SummaryMinor:
 
     Examples:
     
-        1. To get the number of iterations done on the first field, fifth channel, first polarity, during the middle minor cycle:
+        1. To get the number of iterations done on the first field, fifth channel, first stokes plane, during the middle minor cycle:
             field0 = summ_min[0]
             chan   = field0['chans'][5] # channel index doesn't necessarily start at 0
-            pol    = field0['pols'][0]  # polarity index doesn't necessarily start at 0
+            stok   = field0['stoks'][0] # stokes index doesn't necessarily start at 0
             cycle  = field0['ncycs']/2
-            chan5iters = field0[field][chan][pol]['iterDone'][cycle]
+            chan5iters = field0[field][chan][stok]['iterDone'][cycle]
     
         2. To get the number of available channels, and the ids of those channels:
             nchans = len(summ_min[0]["chans"])
@@ -39,12 +39,13 @@ class SummaryMinor:
 
         3. To get the available minor cycle summary statistics:
             field0 = summ_min[0]
-            summaryKeys = field0[field0['chans'][0]][field0['pols'][0]].keys()
+            summaryKeys = field0[field0['chans'][0]][field0['stoks'][0]].keys()
     """
     #                           0           1          2            3              4          5       6      7                  8                9               10                11 "No Mask"      12           13         14           15         16         17              18
-    rowDescriptionsOldOrder = ["iterDone", "peakRes", "modelFlux", "cycleThresh", "deconId", "chan", "pol", "cycleStartIters", "startIterDone", "startPeakRes", "startModelFlux", "startPeakResNM", "peakResNM", "masksum", "mpiServer", "peakMem", "runtime", "multifieldId", "stopCode"]
+    rowDescriptionsOldOrder = ["iterDone", "peakRes", "modelFlux", "cycleThresh", "deconId", "chan", "stok", "cycleStartIters", "startIterDone", "startPeakRes", "startModelFlux", "startPeakResNM", "peakResNM", "masksum", "mpiServer", "peakMem", "runtime", "multifieldId", "stopCode"]
     rowDescriptions13683    = ["iterDone", "peakRes", "modelFlux", "cycleThresh", "deconId", "chan"]
-    rowDescriptions         = ["startIterDone", "iterDone", "startPeakRes", "peakRes", "startModelFlux", "modelFlux", "startPeakResNM", "peakResNM", "cycleThresh", "deconId", "cycleStartIters", "masksum", "mpiServer", "peakMem", "runtime", "multifieldId", "stopCode", "chan", "pol"]
+    # rowDescriptions does not include {"multifieldId", "chan", "stok", "deconId"}, and so the returned dictionary will not include those values in the summary keys
+    rowDescriptions         = ["startIterDone", "iterDone", "startPeakRes", "peakRes", "startModelFlux", "modelFlux", "startPeakResNM", "peakResNM", "cycleThresh", "cycleStartIters", "masksum", "mpiServer", "peakMem", "runtime", "stopCode"]
     rowStartDescs           = ["startIterDone",             "startPeakRes",            "startModelFlux",              "startPeakResNM"]
 
     def convertMatrix(summaryminor_matrix, calc_iterdone_deltas=None, keep_startvals=None):
@@ -54,7 +55,7 @@ class SummaryMinor:
         if summaryminor_matrix.shape[1] == 0:
             return { 0: {
                 'chans': [],
-                'pols': [],
+                'stoks': [],
                 'ncycs': 0
             }}
 
@@ -67,24 +68,23 @@ class SummaryMinor:
         elif len(field_ids) == 1:
             ret[field_ids[0]] = SummaryMinor._convertSingleFieldMatrix(summaryminor_matrix, calc_iterdone_deltas, keep_startvals)
         else:
-            casalog.post("Failed to parse summary minor matrix from tclean. No multifield ids were found.", "SEVERE")
-            return summaryminor_matrix
+            raise RuntimeError("No multifield ids were found. Failed to parse summary minor matrix after tclean finished running.")
 
         # insert convenience information
         for field_id in field_ids:
             chan_ids = sorted(list( ret[field_id].keys() ))
-            pol_ids  = []
+            stok_ids  = []
             nCycles = 0
 
             if len(chan_ids) > 0:
                 chan0  = chan_ids[0]
-                pol_ids = sorted(list( ret[field_id][chan0].keys() ))
-                if len(pol_ids) > 0:
-                    pol0    = pol_ids[0]
-                    nCycles = len( ret[field_id][chan0][pol0]['iterDone'] )
+                stok_ids = sorted(list( ret[field_id][chan0].keys() ))
+                if len(stok_ids) > 0:
+                    stok0    = stok_ids[0]
+                    nCycles = len( ret[field_id][chan0][stok0]['iterDone'] )
 
             ret[field_id]['chans'] = chan_ids
-            ret[field_id]['pols']  = pol_ids
+            ret[field_id]['stoks'] = stok_ids
             ret[field_id]['ncycs'] = nCycles
 
         return ret
@@ -176,7 +176,7 @@ class SummaryMinor:
         return SummaryMinor._getRowStartDescs(SummaryMinor.useSmallSummaryminor())
 
     def indexMinorCycleSummaryBySubimage(matrix):
-        """ Re-indexes matrix from [row,column] to [channel,polarity,row,cycle]. 
+        """ Re-indexes matrix from [row,column] to [channel,stokes,row,cycle]. 
 
         Param matrix: the original matrix to convert.
         """
@@ -186,30 +186,28 @@ class SummaryMinor:
         uss = SummaryMinor.useSmallSummaryminor() # Temporary CAS-13683 workaround
         import sys
         oldChanIdx = SummaryMinor.getRowDescriptionsOldOrder().index("chan")
-        newChanIdx = SummaryMinor.getRowDescriptions().index("chan")
         if not uss:
-            oldPolIdx  = SummaryMinor.getRowDescriptionsOldOrder().index("pol")
-            newPolIdx  = SummaryMinor.getRowDescriptions().index("pol")
+            oldStokIdx  = SummaryMinor.getRowDescriptionsOldOrder().index("stok")
         chans = list(np.sort(np.unique(matrix[oldChanIdx])))
         chans = [int(x) for x in chans]
         if uss:
-            pols = [0]
+            stoks = [0]
         else:
-            pols = list(np.sort(np.unique(matrix[oldPolIdx])))
-            pols = [int(x) for x in pols]
+            stoks = list(np.sort(np.unique(matrix[oldStokIdx])))
+            stoks = [int(x) for x in stoks]
         ncycles = 0
-        if len(chans) > 0 and len(pols) > 0:
-            ncycles = int( ncols / (len(chans)*len(pols)) )
+        if len(chans) > 0 and len(stoks) > 0:
+            ncycles = int( ncols / (len(chans)*len(stoks)) )
             if uss:
                 try:
                     from casampi.MPIEnvironment import MPIEnvironment
                     if MPIEnvironment.is_mpi_enabled:
                         # This is necessary because we may have an odd number of "channels" due to each process getting only a subchunk.
                         # Example:
-                        #     Process 1 gets polarities 0-1, process 2 gets polarity 2
-                        #     Each of them assigns channel id = chan + pol * nsubpols
+                        #     Process 1 gets stokes 0-1, process 2 gets stokes 2
+                        #     Each of them assigns channel id = chan + stok * nsubstoks
                         #     Process 1 assigns channel ids [0,2], Process 2 assigns channel id 0.
-                        # This hack is not needed when not using a small summary minor because we have the extra knowledge of the polarities, instead of mapping polarities + channels onto chunks.
+                        # This hack is not needed when not using a small summary minor because we have the extra knowledge of the stokes, instead of mapping stokes + channels onto chunks.
                         chanslist = matrix[oldChanIdx].tolist()
                         for chan in chans:
                             singlechan_occurances = list(filter(lambda x: x == chan, chanslist))
@@ -217,33 +215,26 @@ class SummaryMinor:
                 except ModuleNotFoundError as e:
                     raise
 
-        # ret is the return dictionary[chans][pols][rows][cycles]
-        # cummulativeCnt counts how many cols we've read for each channel/polarity/row
+        # ret is the return dictionary[chans][stoks][rows][cycles]
+        # cummulativeCnt counts how many cols we've read for each channel/stokes/row
         ret = {desc:[0]*ncycles for desc in SummaryMinor.getRowDescriptions()}
-        # channel and polarity information is in the indexing, don't need to add it to the return dict
-        for desc in ["chan", "pol"]:
-            if desc in ret:
-                del ret[desc]
-        ret = {pol:copy.deepcopy(ret) for pol in pols}
+        ret = {stok:copy.deepcopy(ret) for stok in stoks}
         ret = {chan:copy.deepcopy(ret) for chan in chans}
         cummulativeCnt = copy.deepcopy(ret) # copy ret's structure
 
-        # reindex based on subimage index (aka chan/pol index)
+        # reindex based on subimage index (aka chan/stok index)
         for desc in SummaryMinor.getRowDescriptions():
-            if desc in ["chan", "pol"]:
-                # channel and polarity information is in the indexing, don't need to add it to the return dict
-                continue
             oldRowIdx = SummaryMinor.getRowDescriptionsOldOrder().index(desc)
             for colIdx in range(ncols):
                 chan = int(matrix[oldChanIdx][colIdx])
                 if uss:
-                    pol = 0
+                    stok = 0
                 else:
-                    pol = int(matrix[oldPolIdx][colIdx])
+                    stok = int(matrix[oldStokIdx][colIdx])
                 val = matrix[oldRowIdx][colIdx]
-                cummulativeCol = int(cummulativeCnt[chan][pol][desc][0]) # const 0: cummulativeCnt doesn't make use of 'cycle' index from copied ret structure
-                ret[chan][pol][desc][cummulativeCol] = val
-                cummulativeCnt[chan][pol][desc][0] += 1
+                cummulativeCol = int(cummulativeCnt[chan][stok][desc][0]) # const 0: cummulativeCnt doesn't make use of 'cycle' index from copied ret structure
+                ret[chan][stok][desc][cummulativeCol] = val
+                cummulativeCnt[chan][stok][desc][0] += 1
 
         return ret
 
@@ -255,13 +246,13 @@ class SummaryMinor:
 
         if (calc_iterdone_deltas) and ("startIterDone" in availRows):
             for chan in ret:
-                for pol in ret[chan]:
-                    for cyc in range(len(ret[chan][pol]["startIterDone"])):
-                        ret[chan][pol]["iterDone"][cyc] -= ret[chan][pol]["startIterDone"][cyc]
+                for stok in ret[chan]:
+                    for cyc in range(len(ret[chan][stok]["startIterDone"])):
+                        ret[chan][stok]["iterDone"][cyc] -= ret[chan][stok]["startIterDone"][cyc]
         if not keep_startvals:
             for chan in ret:
-                for pol in ret[chan]:
+                for stok in ret[chan]:
                     for desc in SummaryMinor.getRowStartDescs():
-                        del ret[chan][pol][desc]
+                        del ret[chan][stok][desc]
 
         return ret
