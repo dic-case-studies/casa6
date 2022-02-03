8a0cfd9b63320d150dce5ab3dc7f5374e16a3809
Akeem Wells <awells@nrao.edu>
2022-02-03 12:15:18 -0500

    Remove duplicates and add error checking in cases where file syntax is correct but file does not exist

--- casatestutils/runtest.py
+++ casatestutils/runtest.py
@@ -504,7 +504,19 @@ def run(testnames, branch=None):
             # Copy Tests to Working Directory
             os.makedirs(workdir)
 
-            print("Tests: {}".format(testnames))
+            # Remove Duplicates
+            # Since working directory is based on script name
+            # We need to remove multiple calls to the same script
+            setlist = []
+            for duplicate in list(set([ x.split("/")[-1] for x in testnames])):
+                inlist = True
+                for test in testnames:
+                    if duplicate in test:
+                        if inlist:
+                            setlist.append(test)
+                            inlist = False
+            testnames = setlist
+            print("Tests: {}".format(sorted(testnames)))
             gittest = True
             if branch ==None:
                 branch = 'master'
@@ -573,6 +585,7 @@ def run(testnames, branch=None):
                         print("No Tests to Run")
                         sys.exit()
                     else:
+                        
                         myworkdir = os.getcwd()
                         os.chdir("{}".format(workdir + "{}/".format(test if not test.endswith(".py") else test[:-3])))
                         print("Test Directory: {}".format(os.getcwd()))
@@ -1004,7 +1017,7 @@ if __name__ == "__main__":
                     #print(component, myDict["testGroup"])
                     if component in myDict["testGroup"]:
                         _isComponent = True
-                        testnames.append(myDict["testScript"])
+                        if myDict["testScript"] not in testnames: testnames.append(myDict["testScript"])
                 if not _isComponent:
                     print("No Tests for Component: {}".format(component))
                     no_test_components.append(component)
@@ -1061,8 +1074,9 @@ if __name__ == "__main__":
             for test in tests:
                 try:
                     real_path = os.path.realpath(test)
-                    if ("test_" not in real_path) or  ("test_" not in real_path):
+                    if ("test_" not in real_path) or  ("test_" not in real_path) or ( os.path.exists(real_path) ==False):
                         print("{} is not a Test File".format(test))
+                        continue
                     testnames.append(real_path)
                 except:
                     traceback.print_exc()
@@ -1108,11 +1122,6 @@ if __name__ == "__main__":
                                         tests.append(os.path.join(root, file))
                 testnames = tests
 
-            for test in [x for x in testnames if not os.path.exists(x)]:
-                print("File Does not exist: {}".format(test))
-
-            testnames = [x for x in testnames if os.path.exists(x)]
-            print("Testnames: {}".format(testnames))
             if tests_to_ignore is not None:
                 print("\nTests to Ignore: ",tests_to_ignore )
                 indices = []
